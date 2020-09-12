6f91cf88681b5d939241825725dd4b1a48fe1da1
Тихон Воробьев <tihonvorobev@MacBook-Air-Tihon.local>
2020-09-12 15:12:53 +0300

    task00

---
+++ build/CMakeFiles/enumDevices.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/enumDevices.dir/src/main.cpp.o"
+  "enumDevices"
+  "enumDevices.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/enumDevices.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/libs/clew/CMakeFiles/libclew.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/tihonvorobev/Desktop/GPGPUTasks2020/libs/clew/libclew/ocl_init.cpp" "/Users/tihonvorobev/Desktop/GPGPUTasks2020/build/libs/clew/CMakeFiles/libclew.dir/libclew/ocl_init.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../libs/clew"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
