d66dcc53417f8ff75e0508e8f0dec3f70ba785ea
Olga Kunyavskaya <Oche24912@yandex.ru>
2020-09-27 21:30:57 +0300

    recurceve prefix calc

--- src/cl/max_prefix_sum.cl
+++ src/cl/max_prefix_sum.cl
@@ -48,6 +48,41 @@ __kernel void max_prefix_sum(__global const int* a,
     }
 }
 
+
+__kernel void calc_prefix_sum(__global const int* a,
+                              __global const int* prefix_sum,
+                              __global int* sum,
+                              unsigned int n)
+{
+    const unsigned int index = get_global_id(0);
+    const unsigned int bucket_id = index/WORK_GROUP_SIZE;
+    const unsigned int local_index = get_local_id(0);
+
+    __local int local_a[WORK_GROUP_SIZE];
+
+    if (index >= n) {
+        local_a[local_index] = 0;
+    } else {
+        local_a[local_index] = a[index];
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if (local_index == 0) {
+        int local_sum = prefix_sum[bucket_id];
+
+        for (int i = 0; i < WORK_GROUP_SIZE; ++i) {
+            local_sum += local_a[i];
+            local_a[i] = local_sum;
+        }
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if (index < n) {
+        sum[index] = local_a[local_index];
+    }
+}
+
+
 __kernel void sum_in_bucket(__global const int* a,
                             __global int* sum,
                             unsigned int n)
--- src/main_max_prefix_sum.cpp
+++ src/main_max_prefix_sum.cpp
@@ -17,11 +17,41 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+void calc_prefix_sum(std::vector<int>& a, unsigned int workGroupSize, ocl::Kernel& kernel_sum_in_bucket, ocl::Kernel& kernel_calc_prefix_sum) {
+    if (a.size() < 256*workGroupSize) {
+        for (int i = 1; i < a.size(); ++i) {
+            a[i] += a[i - 1];
+        }
+    } else {
+        unsigned int global_work_size = (a.size() + workGroupSize - 1) / workGroupSize * workGroupSize;
+        std::vector<int> bucket_prefix_sum(a.size()/workGroupSize + 2, 0);
+        gpu::gpu_mem_32i as_gpu;
+        gpu::gpu_mem_32i bucket_sum;
+        gpu::gpu_mem_32i sum;
+        unsigned int n = a.size();
+        as_gpu.resizeN(n);
+        sum.resizeN(n);
+        bucket_sum.resizeN(n/workGroupSize + 1);
+
+        as_gpu.writeN(a.data(), n);
+
+        kernel_sum_in_bucket.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, bucket_sum, n);
+
+        bucket_sum.readN(bucket_prefix_sum.data() + 1, a.size()/workGroupSize + 1);
+        calc_prefix_sum(bucket_prefix_sum, workGroupSize, kernel_sum_in_bucket, kernel_calc_prefix_sum);
+        bucket_sum.writeN(bucket_prefix_sum.data(), n/workGroupSize + 1);
+
+        kernel_calc_prefix_sum.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, bucket_sum, sum, n);
+
+        sum.readN(a.data(), n);
+    }
+}
+
 
 int main(int argc, char **argv)
 {
     int benchmarkingIters = 10;
-    int max_n = (1 << 24);
+    int max_n = (1 << 26);
 
     for (int n = 2; n <= max_n; n *= 2) {
         std::cout << "______________________________________________" << std::endl;
@@ -82,10 +112,12 @@ int main(int argc, char **argv)
 
             ocl::Kernel kernel_max_prefix_sum(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "max_prefix_sum");
             ocl::Kernel kernel_sum_in_bucket(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "sum_in_bucket");
+            ocl::Kernel kernel_calc_prefix_sum(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "calc_prefix_sum");
 
             bool printLog = false;
             kernel_max_prefix_sum.compile(printLog);
             kernel_sum_in_bucket.compile(printLog);
+            kernel_calc_prefix_sum.compile(printLog);
 
             int gpu_sum, gpu_res;
             unsigned int workGroupSize = 128;
@@ -114,9 +146,7 @@ int main(int argc, char **argv)
                 kernel_sum_in_bucket.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, bucket_sum, n);
 
                 bucket_sum.readN(bucket_prefix_sum.data() + 1, n/workGroupSize + 1);
-                for (int i = 1; i < bucket_prefix_sum.size(); ++i) {
-                    bucket_prefix_sum[i] += bucket_prefix_sum[i - 1];
-                }
+                calc_prefix_sum(bucket_prefix_sum, workGroupSize, kernel_sum_in_bucket, kernel_calc_prefix_sum);
                 bucket_sum.writeN(bucket_prefix_sum.data(), n/workGroupSize + 1);
 
                 kernel_max_prefix_sum.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, bucket_sum, max_sum_gpu, res_gpu, n);
