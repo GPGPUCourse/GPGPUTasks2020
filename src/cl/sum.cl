775f9f0a0a951784f4ad33527b1a44eb6720f641
Vergara <vlad07112000@gmail.com>
2020-09-27 22:32:55 +0300

    init ci

--- src/cl/sum.cl
+++ src/cl/sum.cl
@@ -1,1 +1,52 @@
-// TODO
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#line 6
+
+#define WORK_GROUP_SIZE (128)
+#define RANGE_PER_WORK_ITEM (64)
+
+__kernel void summa(__global const unsigned int* a,
+                     __global unsigned int* res,
+                     unsigned int n)
+{
+    unsigned int group_id = get_group_id(0);
+    unsigned int localIndex = get_local_id(0);
+
+    unsigned int index = get_global_id(0);
+    __local unsigned int localSum[WORK_GROUP_SIZE];
+    for (int i = 0; i < RANGE_PER_WORK_ITEM; ++i) {
+        unsigned int id = group_id * WORK_GROUP_SIZE * RANGE_PER_WORK_ITEM 
+                                + i * WORK_GROUP_SIZE + localIndex;
+        if (id < n)
+            localSum[localIndex] += a[id];
+    }
+    unsigned int curShift = WORK_GROUP_SIZE / 2;
+    barrier(CLK_LOCAL_MEM_FENCE);
+    
+    if (localIndex < 64)
+        localSum[localIndex] += localSum[localIndex + 64];
+    barrier(CLK_LOCAL_MEM_FENCE);
+    
+    if (localIndex < 32)
+        localSum[localIndex] += localSum[localIndex + 32];
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (localIndex < 16)
+        localSum[localIndex] += localSum[localIndex + 16];
+    barrier(CLK_LOCAL_MEM_FENCE);
+    int t = 16;
+
+    if (localIndex < 8){
+        localSum[localIndex] = localSum[localIndex + 8];
+        t = 8;
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (localIndex == 0){
+        for (unsigned int i = 1; i < t; i++)
+            localSum[0] += localSum[i];
+        atomic_add(res, localSum[0]);
+    }
+}
--- src/main_sum.cpp
+++ src/main_sum.cpp
@@ -2,6 +2,11 @@
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
 
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
+
+#include "cl/sum_cl.h"
+// #include <assert>
 
 template<typename T>
 void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
@@ -14,19 +19,22 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
-
 int main(int argc, char **argv)
 {
     int benchmarkingIters = 10;
 
     unsigned int reference_sum = 0;
-    unsigned int n = 100*1000*1000;
-    std::vector<unsigned int> as(n, 0);
+    // unsigned int n = 1*1000*1000;
+    unsigned int n = 128 * 64 * 32;
+    std::vector<unsigned int> as(n);
+    long long res = 0;
     FastRandom r(42);
     for (int i = 0; i < n; ++i) {
-        as[i] = (unsigned int) r.next(std::numeric_limits<unsigned int>::max() / n);
+        as[i] = (unsigned int) r.next(0, std::numeric_limits<unsigned int>::max() / n);
+        res += as[i];
         reference_sum += as[i];
     }
+    assert(res == reference_sum);
 
     {
         timer t;
@@ -58,7 +66,36 @@ int main(int argc, char **argv)
     }
 
     {
-        // TODO: implement on OpenCL
-        // gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Context context;
+        context.init(device.device_id_opencl);
+        context.activate();
+     
+        timer t;
+        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+            gpu::gpu_mem_32u gpuA;
+            gpuA.resizeN(n);
+            gpuA.writeN(as.data(), n);
+
+            ocl::Kernel summa(sum_kernel, sum_kernel_length, "summa");
+            summa.compile();
+
+            unsigned int res = 0;
+            gpu::gpu_mem_32u gpuRes;
+            gpuRes.resizeN(1);
+            gpuRes.writeN(&res, 1);
+            unsigned int workGroupSize = 128;
+            unsigned int rangePerWorkItem = 64;
+            unsigned int globalWorkSize = (n + workGroupSize - 1) / workGroupSize * workGroupSize / rangePerWorkItem;
+
+            summa.exec(gpu::WorkSize(workGroupSize, globalWorkSize), gpuA, gpuRes, n);
+
+            gpuRes.readN(&res, 1);
+            EXPECT_THE_SAME(reference_sum, res, "OpenCL result should be consistent!");
+            break;
+            t.nextLap();
+        }
+        std::cout << "OpenCL: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "OpenCL: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
     }
 }
