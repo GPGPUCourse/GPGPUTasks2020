5e0bad3c8a6e15fd95bd520f5ff4fa7afd613612
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2020-09-27 15:59:38 +0300

    Debug 1.2n

--- src/main_max_prefix_sum.cpp
+++ src/main_max_prefix_sum.cpp
@@ -117,10 +117,10 @@ int main(int argc, char **argv)
           BufferSrcFirst->writeN(as.data(), n);
 
           for (int i = 1; i < 5; i++)
-            BuffersArray[i].resizeN(NumberOfGroup * WorkGroupSize * 1.2);
+            BuffersArray[i].resizeN(/*NumberOfGroup * WorkGroupSize*/1.2 * n);
 
           for (int i = 0; i < 2; i++)
-            BuffersArrayInd[i].resizeN(NumberOfGroup * WorkGroupSize * 1.2);
+            BuffersArrayInd[i].resizeN(/*NumberOfGroup * WorkGroupSize*/1.2 * n);
 
           std::vector<int> ResSumArrCPU(OptimalCPU);
           std::vector<int> ResMaxSumArrCPU(OptimalCPU);
