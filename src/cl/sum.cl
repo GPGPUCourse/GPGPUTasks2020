1513005a293b7e9156035c98bbedcc19c786ea25
gnom2134 <gnom21345@gmail.com>
2020-09-29 00:28:48 +0300

    sum is working correctly(not optimised yet)

--- src/cl/sum.cl
+++ src/cl/sum.cl
@@ -1,1 +1,30 @@
-// TODO
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#line 6
+#define WORK_GROUP_SIZE 128
+
+__kernel void sum_k(__global const unsigned int* array, __global unsigned int* res, unsigned int n) {
+    int local_id = get_local_id(0);
+    int global_id = get_global_id(0);
+
+    __local unsigned int local_mem[WORK_GROUP_SIZE];
+    if (global_id < n) {
+        local_mem[local_id] = array[global_id];
+    } else {
+        local_mem[local_id] = 0;
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+    for (int current_n = WORK_GROUP_SIZE; current_n > 1; current_n /= 2) {
+        if (2 * local_id < current_n) {
+            local_mem[local_id] += local_mem[local_id + current_n / 2];
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+    if (local_id == 0) {
+        atomic_add(res, local_mem[local_id]);
+    }
+}
--- src/main_sum.cpp
+++ src/main_sum.cpp
@@ -1,6 +1,10 @@
 #include <libutils/misc.h>
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
+
+#include "cl/sum_cl.h"
 
 
 template<typename T>
@@ -59,6 +63,34 @@ int main(int argc, char **argv)
 
     {
         // TODO: implement on OpenCL
-        // gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Context context;
+        context.init(device.device_id_opencl);
+        context.activate();
+
+        gpu::gpu_mem_32u arr_mem, result;
+        result.resizeN(1);
+        arr_mem.resizeN(n);
+        arr_mem.writeN(as.data(), n);
+
+        ocl::Kernel sum_k(sum_kernel, sum_kernel_length, "sum_k");
+        sum_k.compile();
+
+        unsigned int workGroupSize = 128;
+        unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
+
+        timer t;
+        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+            unsigned int tmp = 0;
+            result.writeN(&tmp, 1);
+            sum_k.exec(gpu::WorkSize(workGroupSize, global_work_size), arr_mem, result, n);
+            t.nextLap();
+        }
+        unsigned int sum = 0;
+        result.readN(&sum, 1);
+        EXPECT_THE_SAME(reference_sum, sum, "GPU result should be consistent!");
+
+        std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "GPU: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
     }
 }
