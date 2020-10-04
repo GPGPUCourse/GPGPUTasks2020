370f53da9dbcdee4c38fcae9fd1eb5e6510e0bc2
denis.konoplev <denis.konoplev@jetbrains.com>
2020-10-05 00:58:30 +0300

    Max prefix sum

--- src/cl/max_prefix_sum.cl
+++ src/cl/max_prefix_sum.cl
@@ -1,1 +1,45 @@
-// TODO
+#define SIZE 128
+__kernel void max_prefix_sum(__global const int* sum,
+                             __global const int* max_sum,
+                             __global const int* index,
+                             unsigned int n,
+                             __global int* new_sum,
+                             __global int* new_max_sum,
+                             __global int* new_index) {
+    __local int local_sum[SIZE];
+    __local int local_max_sum[SIZE];
+    __local int local_index[SIZE];
+
+    const size_t group_id = get_group_id(0);
+    const size_t local_id = get_local_id(0);
+    const size_t global_id = group_id * get_local_size(0) + local_id;
+    if (global_id < n) {
+        local_sum[local_id] = sum[global_id];
+        local_max_sum[local_id] = max_sum[global_id];
+        local_index[local_id] = index[global_id];
+    } else {
+        local_sum[local_id] = 0;
+        local_max_sum[local_id] = 0;
+        local_index[local_id] = 0;
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (local_id == 0) {
+        int tmp_sum = 0;
+        int tmp_max_sum = 0;
+        int tmp_index = 0;
+
+        for (size_t i = 0; i < SIZE; ++i) {
+            if (tmp_sum + local_max_sum[i] > tmp_max_sum) {
+                tmp_max_sum = tmp_sum + local_max_sum[i];
+                tmp_index = local_index[i];
+            }
+            tmp_sum += local_sum[i];
+        }
+
+        new_sum[group_id] = tmp_sum;
+        new_max_sum[group_id] = tmp_max_sum;
+        new_index[group_id] = tmp_index;
+    }
+}
--- src/main_max_prefix_sum.cpp
+++ src/main_max_prefix_sum.cpp
@@ -1,7 +1,10 @@
 #include <libutils/misc.h>
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
 
+#include "cl/max_prefix_sum_cl.h"
 
 template<typename T>
 void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
@@ -17,6 +20,8 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 int main(int argc, char **argv)
 {
+    gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+
     int benchmarkingIters = 10;
     int max_n = (1 << 24);
 
@@ -71,7 +76,69 @@ int main(int argc, char **argv)
         }
 
         {
-            // TODO: implement on OpenCL
+            gpu::Context context;
+            context.init(device.device_id_opencl);
+            context.activate();
+
+            ocl::Kernel kernel(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "max_prefix_sum");
+            kernel.compile();
+
+            std::vector<int> idx(n, 0);
+            for (int i = 0; i < n; ++i) {
+                idx[i] = i + 1;
+            }
+            gpu::gpu_mem_32i sum, new_sum;
+            gpu::gpu_mem_32i max_sum, new_max_sum;
+            gpu::gpu_mem_32i index, new_index;
+
+            sum.resizeN(n);
+            sum.writeN(as.data(), n);
+            max_sum.resizeN(n);
+            max_sum.writeN(as.data(), n);
+            index.resizeN(n);
+            index.writeN(idx.data(), n);
+
+            unsigned int work_group_size = 128;
+            unsigned int work_size = n;
+
+            // resize output data
+            unsigned int out_size = (work_size + work_group_size - 1) / work_group_size;
+            new_sum.resizeN(out_size);
+            new_max_sum.resizeN(out_size);
+            new_index.resizeN(out_size);
+
+            timer t;
+            for (int i = 0; i < benchmarkingIters; ++i) {
+                do {
+                    kernel.exec(gpu::WorkSize(work_group_size, work_size),
+                                sum, max_sum, index,
+                                work_size,
+                                new_sum, new_max_sum, new_index);
+                    work_size = (work_size + work_group_size - 1) / work_group_size;
+
+                    // swap in and out
+                    sum.swap(new_sum);
+                    max_sum.swap(new_max_sum);
+                    index.swap(new_index);
+                    // we can skip resize and rewriting data
+                    // cause we overwrite it in next iteration
+                } while (work_size > 1);
+
+                // read original gpu_data
+                // after last swap it has output data
+                int res_max_sum = 0;
+                max_sum.readN(&res_max_sum, 1);
+
+                int result = 0;
+                index.readN(&result, 1);
+
+                EXPECT_THE_SAME(reference_max_sum, res_max_sum, "GPU result should be consistent!");
+                EXPECT_THE_SAME(reference_result, result, "GPU result should be consistent!");
+                t.nextLap();
+            }
+
+            std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
         }
     }
 }
