806f897abaee5ebee5536a1da5b968a2a067f920
Daniel-mon-Goose <danny-rosso@yandex.ru>
2020-10-05 21:39:10 +0700

    started prefix

--- src/cl/max_prefix_sum.cl
+++ src/cl/max_prefix_sum.cl
@@ -1,1 +1,45 @@
-// TODO
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+#define WORK_GROUP_SIZE 256
+#define VALUES_PER_WORK_ITEM 2
+
+#line 8
+
+__kernel void prefix_sum(__global const int* in, __global int* out, unsigned int n) {
+    unsigned int localId = get_local_id(0);
+    unsigned int groupId = get_group_id(0);
+
+    __local int local_sum[WORK_GROUP_SIZE];
+    __local int local_prefix[WORK_GROUP_SIZE];
+    unsigned int current = VALUES_PER_WORK_ITEM * localId;
+
+    for (int i = 0; i < WORK_GROUP_SIZE; ++i) {
+        local_sum[i] = 0;
+        local_prefix[i] = 0;
+    }
+
+    int sum = 0;
+    for (int i = 0; i < VALUES_PER_WORK_ITEM; ++i) {
+        unsigned int index = i + WORK_GROUP_SIZE * groupId + current;
+        if (index < n) {
+            sum += in[index];
+            if (sum > local_sum[localId]) {
+                local_prefix[localId] = sum;
+            }
+            local_sum[localId] = sum;
+        } else {
+            local_sum[localId] = 0;
+        }
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if (localId == 0) {
+        for (int i = 1; i < WORK_GROUP_SIZE; ++i) {
+            local_prefix[0] = max(local_prefix[0], local_prefix[i] + local_sum[0]);
+            local_sum[0] += local_sum[i];
+        }
+    }
+
+    out[groupId] = local_prefix[0];
+}
--- src/main_max_prefix_sum.cpp
+++ src/main_max_prefix_sum.cpp
@@ -1,7 +1,10 @@
 #include <libutils/misc.h>
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
 
+#include "cl/max_prefix_sum_cl.h"
 
 template<typename T>
 void raiseFail(const T &a, const T &b, const std::string& message, const std::string& filename, int line)
@@ -14,11 +17,27 @@ void raiseFail(const T &a, const T &b, const std::string& message, const std::st
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+/*unsigned int getGlobalSize(unsigned int size, unsigned int workGroupSize) {
+    return std::max(size, workGroupSize);
+}*/
+
+unsigned int getBufferSize(unsigned int size) {
+    return std::max(size, 1u);
+}
+
 
 int main(int argc, char **argv)
 {
-    int benchmarkingIters = 10;
+    int benchmarkingIters = 100;
     int max_n = (1 << 24);
+
+    gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+    gpu::Context context;
+    context.init(device.device_id_opencl);
+    context.activate();
+
+    ocl::Kernel gpu_prefix(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "prefix_sum");
+    gpu_prefix.compile();
 
     for (int n = 2; n <= max_n; n *= 2) {
         std::cout << "______________________________________________" << std::endl;
@@ -71,7 +90,46 @@ int main(int argc, char **argv)
         }
 
         {
-            // TODO: implement on OpenCL
+            /*timer t;
+            unsigned int workGroupSize = 256;
+            unsigned int values_per_item = 2;
+            unsigned int global_work_size = (n + values_per_item - 1) / values_per_item;
+            gpu::gpu_mem_32i as_gpu;
+            as_gpu.resizeN(n);
+            as_gpu.writeN(as.data(), n);
+
+            unsigned int shrinker = workGroupSize * values_per_item;
+
+            gpu::gpu_mem_32i bs_gpu;
+            bs_gpu.resizeN(getBufferSize(n / shrinker));
+            for (int i = 0; i < benchmarkingIters; ++i) {
+
+                int count = 0;
+                for (unsigned int gl_s = global_work_size; gl_s >= 1; gl_s /= shrinker) {
+                    if (count % 2 == 0) {
+                        gpu_prefix.exec(gpu::WorkSize(workGroupSize, gl_s),
+                                        as_gpu, bs_gpu, as_gpu.size());
+                        as_gpu.resizeN(getBufferSize(as_gpu.size() / shrinker / shrinker));
+                    } else {
+                        gpu_prefix.exec(gpu::WorkSize(workGroupSize, gl_s),
+                                        bs_gpu, as_gpu, bs_gpu.size());
+                        bs_gpu.resizeN(getBufferSize(bs_gpu.size() / shrinker / shrinker));
+                    }
+                    count++;
+                }
+
+                int prefix;
+                if (count % 2 == 0) {
+                    bs_gpu.readN(&prefix, 1);
+                } else {
+                    as_gpu.readN(&prefix, 1);
+                }
+
+                EXPECT_THE_SAME(reference_max_sum, prefix, "GPU result should be consistent!");
+                t.nextLap();
+            }
+            std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;*/
         }
     }
 }
