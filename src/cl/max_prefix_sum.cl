4cf562c7cbc35d13c9f290e1d2ca6ffb81b97d92
Plyushchenko <plyushenko@bk.ru>
2020-10-05 16:55:00 +0300

    3.3

--- src/cl/max_prefix_sum.cl
+++ src/cl/max_prefix_sum.cl
@@ -1,1 +1,53 @@
-// TODO
+__kernel void max_prefix_sum(
+        __global const int *input_sum,
+        __global const int *input_max_pref_sum,
+        __global const unsigned int *input_max_pref_index,
+		__local int *local_sum,
+        __local int *local_max_pref_sum,
+        __local unsigned int *local_max_pref_index,
+		unsigned int n, 
+		__global int *sum,
+        __global int *max_pref_sum,
+        __global unsigned int *max_pref_index)
+{
+    const unsigned int global_id = get_global_id(0);
+    const unsigned int local_id = get_local_id(0);
+    const unsigned int group_id = get_group_id(0);
+    const unsigned int group_size = get_local_size(0);
+    if (global_id < n) {
+        local_sum[local_id] = input_sum[global_id];
+        local_max_pref_sum[local_id] = input_max_pref_sum[global_id];
+        local_max_pref_index[local_id] = input_max_pref_index[global_id];
+    } else {
+        local_sum[local_id] = 0;
+        local_max_pref_sum[local_id] = 0;
+        local_max_pref_index[local_id] = 0;
+    }
+    
+    barrier(CLK_LOCAL_MEM_FENCE);
+   
+    for (unsigned int shift = group_size / 2; shift > 0; shift /= 2) {
+        if (local_id < shift) {
+            int left_pref_sum = local_max_pref_sum[2 * local_id];
+            int right_pref_sum = local_max_pref_sum[2 * local_id + 1] + local_sum[2 * local_id];
+            if (left_pref_sum >= right_pref_sum) {
+                local_max_pref_sum[local_id] = left_pref_sum;
+                local_max_pref_index[local_id] = local_max_pref_index[2 * local_id]; 
+            } else {
+                local_max_pref_sum[local_id] = right_pref_sum;
+                local_max_pref_index[local_id] = local_max_pref_index[2 * local_id + 1]; 
+            }
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+        if (local_id < shift) {
+            local_sum[local_id] = local_sum[2 * local_id] + local_sum[2 * local_id + 1];
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+    if (local_id == 0) {
+        sum[group_id] = local_sum[0];
+        max_pref_sum[group_id] = local_max_pref_sum[0];
+        max_pref_index[group_id] = local_max_pref_index[0];
+    }
+}
--- src/main_max_prefix_sum.cpp
+++ src/main_max_prefix_sum.cpp
@@ -1,6 +1,10 @@
 #include <libutils/misc.h>
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
+
+#include "cl/max_prefix_sum_cl.h"
 
 
 template<typename T>
@@ -26,9 +30,11 @@ int main(int argc, char **argv)
         std::cout << "n=" << n << " values in range: [" << (-values_range) << "; " << values_range << "]" << std::endl;
 
         std::vector<int> as(n, 0);
+        std::vector<unsigned int> indices(n, 0);
         FastRandom r(n);
         for (int i = 0; i < n; ++i) {
             as[i] = (unsigned int) r.next(-values_range, values_range);
+            indices[i] = i;
         }
 
         int reference_max_sum;
@@ -69,9 +75,83 @@ int main(int argc, char **argv)
             std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
             std::cout << "CPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
         }
-
+        unsigned int old_n = n;
         {
             // TODO: implement on OpenCL
+            gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+            gpu::Context context;
+            context.init(device.device_id_opencl);
+            context.activate();
+            
+            {
+                ocl::Kernel kernel(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "max_prefix_sum");
+                bool printLog = true;
+                kernel.compile(printLog);
+
+                timer t;
+                
+                gpu::gpu_mem_32i sum_vram[] = {
+                    gpu::gpu_mem_32i::createN(n),
+                    gpu::gpu_mem_32i::createN(n)
+                };
+                gpu::gpu_mem_32i pref_sum_vram[] = {
+                    gpu::gpu_mem_32i::createN(n),
+                    gpu::gpu_mem_32i::createN(n)
+                };
+                gpu::gpu_mem_32u pref_index_vram[] = {
+                    gpu::gpu_mem_32u::createN(n),
+                    gpu::gpu_mem_32u::createN(n)
+                };
+                
+                for (int iter = 0; iter < benchmarkingIters; ++iter) {
+                    n = old_n;
+                    sum_vram[0].writeN(as.data(), n);
+                    sum_vram[0].copyToN(pref_sum_vram[0], n);
+                    pref_index_vram[0].writeN(indices.data(), n);
+                
+                    unsigned int work_group_size = 32;
+                    unsigned int work_groups_number = (n + work_group_size - 1) / work_group_size;
+                    unsigned int sum;
+                    while (true) {
+                        kernel.exec(gpu::WorkSize(work_group_size, work_groups_number * work_group_size),
+                            sum_vram[0],
+                            pref_sum_vram[0],
+                            pref_index_vram[0],
+                            ocl::LocalMem(work_group_size * sizeof(int)),
+                            ocl::LocalMem(work_group_size * sizeof(int)),
+                            ocl::LocalMem(work_group_size * sizeof(unsigned int)),
+                            n,
+                            sum_vram[1],
+                            pref_sum_vram[1],
+                            pref_index_vram[1]
+                        );
+                        if (work_groups_number == 1) {
+                            break;
+                        }
+                        n = work_groups_number;
+                        work_groups_number = (work_groups_number + work_group_size - 1) / work_group_size;
+                        std::swap(sum_vram[0], sum_vram[1]);
+                        std::swap(pref_sum_vram[0], pref_sum_vram[1]);
+                        std::swap(pref_index_vram[0], pref_index_vram[1]);
+                    }
+                    int max_sum;
+                    pref_sum_vram[1].readN(&max_sum, 1);
+                    unsigned int result;
+                    pref_index_vram[1].readN(&result, 1);
+                    if (max_sum < 0) {
+                        max_sum = 0;
+                        result = 0;
+                    } else {
+                        result++;
+                    }
+                    EXPECT_THE_SAME(reference_max_sum, max_sum, "GPU result should be consistent!");
+                    EXPECT_THE_SAME(reference_result, (int)result, "GPU result should be consistent!");
+                    t.nextLap();
+                }
+                std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+                std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
+            }
+            n = old_n;
         }
     }
 }
