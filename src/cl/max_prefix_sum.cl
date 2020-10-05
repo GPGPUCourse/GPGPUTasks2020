86907954d5339cdee5ebd34e96025c5cfd3b21e5
Nikolay <smir.nick2000@gmail.com>
2020-10-05 19:05:25 +0300

    Solve task03/max_prefix_sum

--- src/cl/max_prefix_sum.cl
+++ src/cl/max_prefix_sum.cl
@@ -1,1 +1,83 @@
-// TODO
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#line 6
+
+
+/*
+__kernel void max_prefix_sum_tree(__global int *inout_sum, __global int *inout_prefix,  unsigned int n)
+{
+    __local int max_prefix[2][WORK_GROUP_SIZE];
+    __local int sum[2][WORK_GROUP_SIZE];
+
+    int id = get_global_id(0);
+    int local_id = get_local_id(0);
+    
+    sum[0][local_id] = inout_sum[id];
+    max_prefix[0][local_id] = inout_prefix[id];
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    for (int i = WORK_GROUP_SIZE / 2; i >= 1; i /= 2) {
+        if (local_id < i) {
+            int id1 = local_id * 2;
+            int id2 = local_id * 2 + 1;
+    
+            int s1 = sumA[id1];
+            int s2 = sumA[id2];
+    
+            sumB[local_id] = s1 + s2;
+            max_prefixB[local_id] = max(max_prefixA[id1], s1 + max_prefixA[id2]);
+        }
+        
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+    
+    if (local_id == 0) {
+        int out_id = id / WORK_GROUP_SIZE;
+        inout_sum[out_id] = sumA[0];
+        inout_prefix[out_id] = max_prefixA[0];
+    }
+}*/
+
+
+__kernel void max_prefix_sum(__global int *in_sum, __global int *in_prefix, __global int *in_pid,
+                             __global int *out_sum, __global int *out_prefix, __global int *out_pid,
+                             unsigned int n)
+{
+    __local int max_prefix[WORK_GROUP_SIZE];
+    __local int sum[WORK_GROUP_SIZE];
+    __local int pid[WORK_GROUP_SIZE];
+    
+    int id = get_global_id(0);
+    int local_id = get_local_id(0);
+    
+    sum[local_id] = in_sum[id];
+    max_prefix[local_id] = in_prefix[id];
+    pid[local_id] = in_pid[id];
+    
+    barrier(CLK_LOCAL_MEM_FENCE);
+    
+    if (local_id == 0) {
+        int mySum = 0;
+        int myPref = 0;
+        int myPid = 0;
+        
+        for (int i = 0; i < min((int) WORK_GROUP_SIZE, (int) (n - id)); i++) {
+            if (myPref < mySum + max_prefix[i]) {
+                myPref = mySum + max_prefix[i];
+                myPid = pid[i];
+            }
+            
+            //myPref = max((int) (myPref), (int) (mySum + max_prefix[i]));
+            mySum += sum[i];
+        }
+        
+        int out_id = id / WORK_GROUP_SIZE;
+        
+        out_sum[out_id] = mySum;
+        out_prefix[out_id] = myPref;
+        out_pid[out_id] = myPid;
+    }
+}
--- src/main_max_prefix_sum.cpp
+++ src/main_max_prefix_sum.cpp
@@ -1,6 +1,9 @@
 #include <libutils/misc.h>
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
+#include "cl/max_prefix_sum_cl.h"
 
 
 template<typename T>
@@ -17,6 +20,17 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 int main(int argc, char **argv)
 {
+    gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+    
+    gpu::Context context;
+    context.init(device.device_id_opencl);
+    context.activate();
+    
+    const unsigned int workGroupSize = 256;
+    std::string defines = "-DWORK_GROUP_SIZE=" + std::to_string(workGroupSize);
+    ocl::Kernel kernel(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "max_prefix_sum", defines);
+    kernel.compile(false);
+    
     int benchmarkingIters = 10;
     int max_n = (1 << 24);
 
@@ -28,7 +42,7 @@ int main(int argc, char **argv)
         std::vector<int> as(n, 0);
         FastRandom r(n);
         for (int i = 0; i < n; ++i) {
-            as[i] = (unsigned int) r.next(-values_range, values_range);
+            as[i] = (unsigned int) r.next(-values_range, values_range); // ?? confusing cast from (int) to (unsigned int) and then back to (int)
         }
 
         int reference_max_sum;
@@ -71,7 +85,68 @@ int main(int argc, char **argv)
         }
 
         {
-            // TODO: implement on OpenCL
+            //unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
+            unsigned int global_work_size = n;
+    
+            int out_sum;
+            int out_prefix;
+            int out_pid;
+            
+            gpu::gpu_mem_32i in_sum_buf, in_prefix_buf, in_pid_buf;
+            gpu::gpu_mem_32i out_sum_buf, out_prefix_buf, out_pid_buf;
+            
+            in_sum_buf.resizeN(global_work_size);
+            in_prefix_buf.resizeN(global_work_size);
+            in_pid_buf.resizeN(global_work_size);
+            
+            out_sum_buf.resizeN((global_work_size + workGroupSize - 1) / workGroupSize);
+            out_prefix_buf.resizeN((global_work_size + workGroupSize - 1) / workGroupSize);
+            out_pid_buf.resizeN((global_work_size + workGroupSize - 1) / workGroupSize);
+    
+            std::vector<int> range(global_work_size);
+            for (int i = 0; i < global_work_size; i++) range[i] = i + 1;
+    
+            
+            
+            timer t;
+            for (int iter = 0; iter < benchmarkingIters; iter++) {
+                in_sum_buf.writeN(as.data(), global_work_size);
+                in_prefix_buf.writeN(as.data(), global_work_size);
+                in_pid_buf.writeN(range.data(), global_work_size);
+                
+                gpu::gpu_mem_32i * p_in_sum = &in_sum_buf;
+                gpu::gpu_mem_32i * p_out_sum = &out_sum_buf;
+                
+                gpu::gpu_mem_32i * p_in_pref = &in_prefix_buf;
+                gpu::gpu_mem_32i * p_out_pref = &out_prefix_buf;
+                
+                gpu::gpu_mem_32i * p_in_pid = &in_pid_buf;
+                gpu::gpu_mem_32i * p_out_pid = &out_pid_buf;
+                
+                for (unsigned int i = global_work_size; i > 1; i = (i + workGroupSize - 1) / workGroupSize)
+                {
+                    kernel.exec(gpu::WorkSize(workGroupSize, i),
+                                *p_in_sum, *p_in_pref, *p_in_pid,
+                                *p_out_sum, *p_out_pref, *p_out_pid,
+                                i);
+                    
+                    unsigned int out_size = (i + workGroupSize - 1) / workGroupSize;
+                    
+                    std::swap(p_in_sum, p_out_sum);
+                    std::swap(p_in_pref, p_out_pref);
+                    std::swap(p_in_pid, p_out_pid);
+                }
+                
+                p_in_pid->readN(&out_pid, 1);
+                p_in_pref->readN(&out_prefix, 1);
+                
+                EXPECT_THE_SAME(reference_max_sum, out_prefix, "GPU result should be consistent!");
+                EXPECT_THE_SAME(reference_result, out_pid, "GPU result should be consistent!");
+                t.nextLap();
+            }
+    
+            std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
         }
     }
 }
