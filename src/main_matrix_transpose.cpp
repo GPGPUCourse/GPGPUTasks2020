047289f11e191769c50c91216bbcb8a208b807c5
Tur Vsevolod <60890623+turvd@users.noreply.github.com>
2020-10-11 19:18:13 +0300

    Add files via upload

--- src/main_matrix_transpose.cpp
+++ src/main_matrix_transpose.cpp
@@ -20,8 +20,8 @@ int main(int argc, char **argv)
     context.activate();
 
     int benchmarkingIters = 10;
-    unsigned int M = 1024;
-    unsigned int K = 1024;
+    unsigned int M = 4*1024;///1024;
+    unsigned int K = 4*1024;///1024;
 
     std::vector<float> as(M*K, 0);
     std::vector<float> as_t(M*K, 0);
@@ -32,7 +32,7 @@ int main(int argc, char **argv)
     }
     std::cout << "Data generated for M=" << M << ", K=" << K << "!" << std::endl;
 
-    /*
+
     gpu::gpu_mem_32f as_gpu, as_t_gpu;
     as_gpu.resizeN(M*K);
     as_t_gpu.resizeN(K*M);
@@ -46,9 +46,12 @@ int main(int argc, char **argv)
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             // TODO
-            unsigned int work_group_size = 128;
-            unsigned int global_work_size = ...;
-            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), as_gpu, as_t_gpu, M, K);
+            unsigned int work_group_size = 16;
+            unsigned int global_work_size_x = ((M+work_group_size-1)/work_group_size)*work_group_size;
+			unsigned int global_work_size_y = ((K+work_group_size-1)/work_group_size)*work_group_size;
+            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size, work_group_size,
+										 global_work_size_x, global_work_size_y),
+										 as_gpu, as_t_gpu, M, K);
 
             t.nextLap();
         }
@@ -57,7 +60,8 @@ int main(int argc, char **argv)
     }
 
     as_t_gpu.readN(as_t.data(), M*K);
-
+	
+	
     // Проверяем корректность результатов
     for (int j = 0; j < M; ++j) {
         for (int i = 0; i < K; ++i) {
@@ -69,7 +73,7 @@ int main(int argc, char **argv)
             }
         }
     }
-    */
+    
 
     return 0;
 }
