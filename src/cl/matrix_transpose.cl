af8d3d18e777771b4624c4b7f867b53975b6d527
ArtyomGrachev <grach-t@ya.ru>
2020-10-12 04:33:39 +0300

    Task4.1

--- src/cl/matrix_transpose.cl
+++ src/cl/matrix_transpose.cl
@@ -1,4 +1,31 @@
-__kernel void matrix_transpose(...)
-{
-    // TODO
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#line 6
+
+#ifndef TILE_SIZE
+    #define TILE_SIZE 16
+#endif
+__kernel void matrix_transpose(__global const float *matrix, __global float *matrix_t,
+                               const unsigned int m, const unsigned int k) { // matrix with size m x k
+
+    const unsigned int col_idx = get_global_id(0); // work_item id in original matrix
+    const unsigned int row_idx = get_global_id(1);
+
+    __local float tile[TILE_SIZE][TILE_SIZE + 1]; // optimizing vs. bank-conflict
+
+    const unsigned int local_col_idx = get_local_id(0); // work_item id in tile
+    const unsigned int local_row_idx = get_local_id(1);
+
+    if (row_idx < m && col_idx < k)
+        tile[local_row_idx][local_col_idx] = matrix[row_idx * k + col_idx];
+    barrier(CLK_LOCAL_MEM_FENCE); // Prevents write after read
+
+    if (row_idx < m && col_idx < k) {
+        matrix_t[local_col_idx >= local_row_idx ?
+                 m * (col_idx - (local_col_idx - local_row_idx)) + row_idx + (local_col_idx - local_row_idx) :
+                 m * (col_idx + (local_row_idx - local_col_idx)) + row_idx - (local_row_idx - local_col_idx)
+                 ] = tile[local_col_idx][local_row_idx];
+    }
 }
--- src/main_matrix_transpose.cpp
+++ src/main_matrix_transpose.cpp
@@ -20,8 +20,8 @@ int main(int argc, char **argv)
     context.activate();
 
     int benchmarkingIters = 10;
-    unsigned int M = 1024;
-    unsigned int K = 1024;
+    unsigned int M = 4096;
+    unsigned int K = 4096;
 
     std::vector<float> as(M*K, 0);
     std::vector<float> as_t(M*K, 0);
@@ -32,7 +32,7 @@ int main(int argc, char **argv)
     }
     std::cout << "Data generated for M=" << M << ", K=" << K << "!" << std::endl;
 
-    /*
+
     gpu::gpu_mem_32f as_gpu, as_t_gpu;
     as_gpu.resizeN(M*K);
     as_t_gpu.resizeN(K*M);
@@ -43,20 +43,24 @@ int main(int argc, char **argv)
     matrix_transpose_kernel.compile();
 
     {
+        // set-up 2D NDRange
+        const unsigned int work_group_size_x = 16;
+        const unsigned int global_work_size_x = (K + work_group_size_x - 1) / work_group_size_x * work_group_size_x;
+        const unsigned int work_group_size_y = 16;
+        const unsigned int global_work_size_y = (M + work_group_size_y - 1) / work_group_size_y * work_group_size_y;
+
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            // TODO
-            unsigned int work_group_size = 128;
-            unsigned int global_work_size = ...;
-            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), as_gpu, as_t_gpu, M, K);
-
+            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size_x, work_group_size_y,
+                                                       global_work_size_x, global_work_size_y),
+                                         as_gpu, as_t_gpu, M, K);
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << M*K/1000.0/1000.0 / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "GPU: " << M*K/1000.0/1000.0/t.lapAvg() << " millions/s" << std::endl;
     }
 
-    as_t_gpu.readN(as_t.data(), M*K);
+    as_t_gpu.readN(as_t.data(), M * K);
 
     // Проверяем корректность результатов
     for (int j = 0; j < M; ++j) {
@@ -69,7 +73,6 @@ int main(int argc, char **argv)
             }
         }
     }
-    */
 
     return 0;
 }
