f8664c4ebd5975d8550712278e92fdfba486e981
wlad111 <wlad962961@gmail.com>
2020-10-12 05:11:49 +0300

    multiplication

--- src/cl/matrix_multiplication.cl
+++ src/cl/matrix_multiplication.cl
@@ -1,4 +1,33 @@
-__kernel void matrix_multiplication(...)
+#define TILE_SIZE 16
+
+__kernel void matrix_multiplication(__global float* a, __global float* b, __global float* c,
+                                    unsigned int M, unsigned int K, unsigned int N)
 {
-    // TODO
+    int i = get_global_id(1);
+    int j = get_global_id(0);
+
+    if (i * N + j >= N * M) {
+        return;
+    }
+
+    __local float tileA[TILE_SIZE][TILE_SIZE];
+    __local float tileB[TILE_SIZE][TILE_SIZE];
+
+    int local_i = get_local_id(1);
+    int local_j = get_local_id(0);
+
+    float c_val = 0.0f;
+
+    for (int tileK = 0; tileK * TILE_SIZE < K; tileK++) {
+        tileA[local_i][local_j] = a[i * K + (tileK * TILE_SIZE + local_j)];
+        tileB[local_i][local_j] = b[(tileK * TILE_SIZE + local_i) * N + j];
+
+        barrier(CLK_LOCAL_MEM_FENCE);
+
+        for (int k = 0; k < TILE_SIZE; k++) {
+            c_val += tileA[local_i][k] * tileB[k][local_j];
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+    c[i * N + j] = c_val;
 }
--- src/main_matrix_multiplication.cpp
+++ src/main_matrix_multiplication.cpp
@@ -58,7 +58,7 @@ int main(int argc, char **argv)
 
     const std::vector<float> cs_cpu_reference = cs;
 
-    /*
+
     gpu::gpu_mem_32f as_gpu, bs_gpu, cs_gpu;
     as_gpu.resizeN(M*K);
     bs_gpu.resizeN(K*N);
@@ -74,9 +74,10 @@ int main(int argc, char **argv)
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             // TODO
-            unsigned int work_group_size = 128;
-            unsigned int global_work_size = ...;
-            matrix_multiplication_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), as_gpu, bs_gpu, cs_gpu, M, K, N);
+            unsigned int work_group_size = 16;
+            unsigned int global_work_sizeX = ceil(double(N) / work_group_size) * work_group_size;
+            unsigned int global_work_sizeY = ceil(double(M) / work_group_size) * work_group_size;
+            matrix_multiplication_kernel.exec(gpu::WorkSize(work_group_size, work_group_size, global_work_sizeX, global_work_sizeY), as_gpu, bs_gpu, cs_gpu, M, K, N);
 
             t.nextLap();
         }
@@ -85,7 +86,7 @@ int main(int argc, char **argv)
     }
 
     cs_gpu.readN(cs.data(), M*N);
-    */
+
 
     // Проверяем корректность результатов
     double diff_sum = 0;
