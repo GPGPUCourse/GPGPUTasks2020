4d1201dfbfb9cba45859d953fcc7bc3a4a5a9228
Daniel-mon-Goose <danny-rosso@yandex.ru>
2020-10-12 16:02:05 +0700

    transpose done

--- src/cl/matrix_transpose.cl
+++ src/cl/matrix_transpose.cl
@@ -1,4 +1,32 @@
-__kernel void matrix_transpose(...)
-{
-    // TODO
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#line 6
+#define TILE_SIZE 16
+
+__kernel void matrix_transpose(__global float* a, __global float* at, unsigned int m, unsigned int k) {
+    int x = get_global_id(0);
+    int y = get_global_id(1);
+
+    int local_x = get_local_id(0);
+    int local_y = get_local_id(1);
+
+    int group_x = get_group_id(0);
+    int group_y = get_group_id(1);
+
+    __local float tile[TILE_SIZE + 1][TILE_SIZE + 1];
+
+    if (x < k && y < m) {
+        tile[local_y][local_x] = a[y * k + x];
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+
+    int new_arr_x = group_y * get_local_size(0) + local_x;
+    int new_arr_y = group_x * get_local_size(1) + local_y;
+
+    if (x < k && y < m) {
+        at[new_arr_y * m + new_arr_x] = tile[local_x][local_y];
+    }
 }
--- src/main_matrix_transpose.cpp
+++ src/main_matrix_transpose.cpp
@@ -19,20 +19,20 @@ int main(int argc, char **argv)
     context.init(device.device_id_opencl);
     context.activate();
 
-    int benchmarkingIters = 10;
-    unsigned int M = 1024;
-    unsigned int K = 1024;
+    int benchmarkingIters = 200;
+    unsigned int M = 4096;
+    unsigned int K = 4096;
 
     std::vector<float> as(M*K, 0);
     std::vector<float> as_t(M*K, 0);
 
-    FastRandom r(M+K);
+    FastRandom r(43);
     for (unsigned int i = 0; i < as.size(); ++i) {
         as[i] = r.nextf();
     }
     std::cout << "Data generated for M=" << M << ", K=" << K << "!" << std::endl;
 
-    /*
+
     gpu::gpu_mem_32f as_gpu, as_t_gpu;
     as_gpu.resizeN(M*K);
     as_t_gpu.resizeN(K*M);
@@ -46,9 +46,12 @@ int main(int argc, char **argv)
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             // TODO
-            unsigned int work_group_size = 128;
-            unsigned int global_work_size = ...;
-            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), as_gpu, as_t_gpu, M, K);
+            unsigned int group_size_side = 16;
+            unsigned int global_work_size_x = (K + group_size_side - 1) / group_size_side * group_size_side;
+            unsigned int global_work_size_y = (M + group_size_side - 1) / group_size_side * group_size_side;
+            matrix_transpose_kernel.exec(gpu::WorkSize(group_size_side, group_size_side,
+                                                       global_work_size_x, global_work_size_y),
+                                         as_gpu, as_t_gpu, M, K);
 
             t.nextLap();
         }
@@ -69,7 +72,7 @@ int main(int argc, char **argv)
             }
         }
     }
-    */
+
 
     return 0;
 }
