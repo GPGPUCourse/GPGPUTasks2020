92c21978d5638ee71e283eb77b80008f8ee24049
tatkir <tatyanakireeva17@gmail.com>
2020-10-12 23:51:01 +0700

    Create kernel to transpose matrix

--- src/cl/matrix_transpose.cl
+++ src/cl/matrix_transpose.cl
@@ -1,4 +1,45 @@
-__kernel void matrix_transpose(...)
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#line 6
+
+#define TILE_SIZE 16
+__kernel void naive_matrix_transpose(__global const float* a,
+                                     __global       float* at,
+                                     unsigned int m, 
+                                     unsigned int k)
 {
-    // TODO
+    int global_i = get_global_id(0);
+    int global_j = get_global_id(1);
+
+    if (global_i < k && global_j < m)
+    {
+        float x = a[global_j * k + global_i];
+        at[global_i * m + global_j] = x;
+    }
+}
+
+__kernel void matrix_transpose(__global const float* a,
+                               __global       float* at,
+                               unsigned int m, 
+                               unsigned int k)
+{
+    int global_i = get_global_id(0);
+    int global_j = get_global_id(1);
+
+    __local float tile[TILE_SIZE * TILE_SIZE];
+    int local_i = get_local_id(0);
+    int local_j = get_local_id(1);
+
+    if (global_i < k && global_j < m)
+        tile[local_j * TILE_SIZE + local_i] = a[global_j * k + global_i];
+
+    barrier(CLK_LOCAL_MEM_FENCE); 
+
+    int group_i = get_group_id(0);
+    int group_j = get_group_id(1);
+
+    if (global_i < k && global_j < m)
+        at[(group_i * TILE_SIZE + local_j) * m  + group_j * TILE_SIZE + local_i] = tile[local_i * TILE_SIZE + local_j];
 }
--- src/main_matrix_transpose.cpp
+++ src/main_matrix_transpose.cpp
@@ -20,8 +20,8 @@ int main(int argc, char **argv)
     context.activate();
 
     int benchmarkingIters = 10;
-    unsigned int M = 1024;
-    unsigned int K = 1024;
+    unsigned int M = 2048;
+    unsigned int K = 2048;
 
     std::vector<float> as(M*K, 0);
     std::vector<float> as_t(M*K, 0);
@@ -32,12 +32,59 @@ int main(int argc, char **argv)
     }
     std::cout << "Data generated for M=" << M << ", K=" << K << "!" << std::endl;
 
-    /*
     gpu::gpu_mem_32f as_gpu, as_t_gpu;
     as_gpu.resizeN(M*K);
     as_t_gpu.resizeN(K*M);
 
     as_gpu.writeN(as.data(), M*K);
+
+    unsigned int work_group_size = 16;
+    unsigned int global_work_size_0 = (K + work_group_size - 1) / work_group_size * work_group_size;
+    unsigned int global_work_size_1 = (M + work_group_size - 1) / work_group_size * work_group_size;
+    //naive matrix transpose
+    ocl::Kernel naive_matrix_transpose_kernel(matrix_transpose, matrix_transpose_length, "naive_matrix_transpose");
+    naive_matrix_transpose_kernel.compile();
+
+    {
+        timer t;
+        for (int iter = 0; iter < benchmarkingIters; ++iter)
+        {
+            // TODO
+
+            naive_matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size, work_group_size, global_work_size_0, global_work_size_1), as_gpu, as_t_gpu, M, K);
+
+            t.nextLap();
+        }
+
+        as_t_gpu.readN(as_t.data(), M * K);
+        std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "GPU: " << M * K / 1000.0 / 1000.0 / t.lapAvg() << " millions/s" << std::endl;
+    }
+
+
+    // Проверяем корректность результатов
+    {
+        bool isSame = true;
+        for (int j = 0; j < M; ++j)
+        {
+            for (int i = 0; i < K; ++i)
+            {
+                float a = as[j * K + i];
+                float b = as_t[i * M + j];
+                if (a != b)
+                {
+                    std::cerr << "Not the same!" << std::endl;
+                    isSame = false;
+                    break;
+                }
+            }
+            if (!isSame)
+                break;
+        }
+
+        if (isSame)
+            std::cout << "The same result!" << std::endl;
+    }
 
     ocl::Kernel matrix_transpose_kernel(matrix_transpose, matrix_transpose_length, "matrix_transpose");
     matrix_transpose_kernel.compile();
@@ -46,30 +93,41 @@ int main(int argc, char **argv)
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             // TODO
-            unsigned int work_group_size = 128;
-            unsigned int global_work_size = ...;
-            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), as_gpu, as_t_gpu, M, K);
+
+            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size, work_group_size, global_work_size_0, global_work_size_1), as_gpu, as_t_gpu, M, K);
 
             t.nextLap();
         }
+
+        as_t_gpu.readN(as_t.data(), M * K);
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << M*K/1000.0/1000.0 / t.lapAvg() << " millions/s" << std::endl;
     }
 
-    as_t_gpu.readN(as_t.data(), M*K);
 
     // Проверяем корректность результатов
-    for (int j = 0; j < M; ++j) {
-        for (int i = 0; i < K; ++i) {
-            float a = as[j * K + i];
-            float b = as_t[i * M + j];
-            if (a != b) {
-                std::cerr << "Not the same!" << std::endl;
-                return 1;
+    {
+        bool isSame = true;
+        for (int j = 0; j < M; ++j)
+        {
+            for (int i = 0; i < K; ++i)
+            {
+                float a = as[j * K + i];
+                float b = as_t[i * M + j];
+                if (a != b)
+                {
+                    std::cerr << "Not the same!" << std::endl;
+                    isSame = false;
+                    break;
+                }
             }
+            if (!isSame)
+                break;
         }
+
+        if (isSame)
+            std::cout << "The same result!" << std::endl;
     }
-    */
 
     return 0;
 }
