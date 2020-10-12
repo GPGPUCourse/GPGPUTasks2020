1cf5c169f2f06e5824a57952677b5378069f3e77
Daniil Smirnov <dansmi97@gmail.com>
2020-10-12 05:05:00 +0300

    Implemented 4.2

--- src/cl/matrix_multiplication.cl
+++ src/cl/matrix_multiplication.cl
@@ -1,4 +1,89 @@
-__kernel void matrix_multiplication(...)
-{
-    // TODO
+#define TILE_SIDE WARP_SIZE
+
+__kernel void matrix_multiplication(
+    __global const float *a, __global const float *b_transposed, __global float *c, 
+    const unsigned Y, const unsigned Z, const unsigned X
+) {
+    // WG x WG groups
+    const size_t sideOfGroup = get_local_size(1); // same as local_size(0)
+
+    const size_t yInGroup = get_local_id(0);
+    const size_t xInGroup = get_local_id(1);
+
+    // correspond to WARP x WARP == (k * WG) x (k * WG) tiles
+    const size_t yGroup = get_group_id(0);
+    const size_t xGroup = get_group_id(1);
+
+    // mapping from group index to position from tile start
+    const size_t indexInGroup = yInGroup * sideOfGroup + xInGroup;
+    const size_t xShiftInTile = indexInGroup % TILE_SIDE; 
+    const size_t yShiftInTile = indexInGroup / TILE_SIDE; 
+
+    // iterations
+    const size_t zTiles = (Z + TILE_SIDE - 1) / TILE_SIDE;
+    const size_t warpCount = sideOfGroup * sideOfGroup / TILE_SIDE;
+
+    __local float a_tile[TILE_SIDE][TILE_SIDE + 1]; // +1 for memory bank shifting
+    __local float b_tile[TILE_SIDE][TILE_SIDE + 1];
+    __local float c_tile[TILE_SIDE][TILE_SIDE + 1];
+
+    // 0. initialize result
+    for (size_t rowsProcessed = 0; rowsProcessed < TILE_SIDE; rowsProcessed += warpCount) {
+        const size_t yInTile = rowsProcessed + yShiftInTile;
+        const size_t xInTile =                 xShiftInTile;
+
+        c_tile[yInTile][xInTile] = 0;
+    }
+
+    for (size_t zTile = 0; zTile < zTiles; ++zTile) {
+        // 1. load tiles to local memory
+        for (size_t rowsProcessed = 0; rowsProcessed < TILE_SIDE; rowsProcessed += warpCount) {
+            const size_t yInTile = rowsProcessed + yShiftInTile;
+            const size_t xInTile =                 xShiftInTile;
+
+            const size_t yGlobal = yGroup * TILE_SIDE + yInTile;
+            const size_t xGlobal = zTile  * TILE_SIDE + xInTile;
+            
+            if (yGlobal < Y && xGlobal < Z)
+                a_tile[yInTile][xInTile] = a[yGlobal * Z + xGlobal];
+            else
+                a_tile[yInTile][xInTile] = 0;
+        }
+
+        for (size_t rowsProcessed = 0; rowsProcessed < TILE_SIDE; rowsProcessed += warpCount) {
+            const size_t yInTile = rowsProcessed + yShiftInTile;
+            const size_t xInTile =                 xShiftInTile;
+
+            const size_t yGlobal = xGroup * TILE_SIDE + yInTile;
+            const size_t xGlobal = zTile  * TILE_SIDE + xInTile;
+        
+            if (yGlobal < X && xGlobal < Z)
+                b_tile[yInTile][xInTile] = b_transposed[yGlobal * Z + xGlobal];
+            else
+                b_tile[yInTile][xInTile] = 0;
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+
+        // 2. multiply them
+        for (size_t rowsProcessed = 0; rowsProcessed < TILE_SIDE; rowsProcessed += warpCount) {
+            const size_t yInTile = rowsProcessed + yShiftInTile;
+            const size_t xInTile =                 xShiftInTile;
+
+            for (size_t zInTile = 0; zInTile < TILE_SIDE; ++zInTile)
+                c_tile[yInTile][xInTile] += a_tile[yInTile][zInTile] * b_tile[xInTile][zInTile];
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+    // 3. write result to global memory
+    for (size_t rowsProcessed = 0; rowsProcessed < TILE_SIDE; rowsProcessed += warpCount) {
+        const size_t yInTile = rowsProcessed + yShiftInTile;
+        const size_t xInTile =                 xShiftInTile;
+
+        const size_t yGlobal = yGroup * TILE_SIDE + yInTile;
+        const size_t xGlobal = xGroup * TILE_SIDE + xInTile;
+        
+        if (yGlobal < Y && xGlobal < X)
+            c[yGlobal * X + xGlobal] = c_tile[yInTile][xInTile];
+    }
 }
--- src/main_matrix_multiplication.cpp
+++ src/main_matrix_multiplication.cpp
@@ -2,14 +2,19 @@
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
 #include <libgpu/context.h>
+#include <libgpu/device.h>
 #include <libgpu/shared_device_buffer.h>
+#include <libgpu/opencl/device_info.h>
 
+#include "cl/matrix_transpose_cl.h"
 #include "cl/matrix_multiplication_cl.h"
 
+#include <cassert>
 #include <vector>
 #include <iostream>
 #include <stdexcept>
 
+//#define WITH_CPU
 
 int main(int argc, char **argv)
 {
@@ -38,6 +43,7 @@ int main(int argc, char **argv)
     }
     std::cout << "Data generated for M=" << M << ", K=" << K << ", N=" << N << "!" << std::endl;
 
+#ifdef WITH_CPU
     {
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
@@ -55,28 +61,69 @@ int main(int argc, char **argv)
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << gflops / t.lapAvg() << " GFlops" << std::endl;
     }
+#endif
 
     const std::vector<float> cs_cpu_reference = cs;
 
-    /*
-    gpu::gpu_mem_32f as_gpu, bs_gpu, cs_gpu;
+    gpu::gpu_mem_32f as_gpu, bs_gpu, bs_t_gpu, cs_gpu;
     as_gpu.resizeN(M*K);
     bs_gpu.resizeN(K*N);
+    bs_t_gpu.resizeN(N*K);
     cs_gpu.resizeN(M*N);
 
     as_gpu.writeN(as.data(), M*K);
     bs_gpu.writeN(bs.data(), K*N);
 
+    ocl::Kernel matrix_transpose_kernel(matrix_transpose, matrix_transpose_length, "matrix_transpose");
+    matrix_transpose_kernel.compile();
+
     ocl::Kernel matrix_multiplication_kernel(matrix_multiplication, matrix_multiplication_length, "matrix_multiplication");
     matrix_multiplication_kernel.compile();
 
     {
+        ocl::DeviceInfo deviceInfo;
+        deviceInfo.init(device.device_id_opencl);
+        
+        size_t work_group_side = 1;
+        while ((work_group_side * work_group_side << 2) <= deviceInfo.max_workgroup_size) {
+            work_group_side <<= 1;
+        }
+
+        const size_t warp_size = deviceInfo.warp_size != 0 ? deviceInfo.warp_size : deviceInfo.wavefront_width;
+        assert(warp_size != 0 && "warp size is not zero");
+
+        // we want tiles of WARP_SIZE x WARP_SIZE which will be filled line by line 
+        // by whole warps and then subdivided into smaller WG_SIDE x WG_SIDE for transposition
+        assert(work_group_side * work_group_side % warp_size == 0 && "wrong assumptions");
+        assert(work_group_side < warp_size                        && "wrong assumptions");
+
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            // TODO
-            unsigned int work_group_size = 128;
-            unsigned int global_work_size = ...;
-            matrix_multiplication_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), as_gpu, bs_gpu, cs_gpu, M, K, N);
+            {
+                const size_t global_tiles_x = (K + warp_size - 1) / warp_size;
+                const size_t global_tiles_y = (N + warp_size - 1) / warp_size;
+
+                const size_t global_work_size_x = global_tiles_x * work_group_side;
+                const size_t global_work_size_y = global_tiles_y * work_group_side;
+
+                matrix_transpose_kernel.exec(
+                    gpu::WorkSize(work_group_side, work_group_side, global_work_size_x, global_work_size_y), 
+                    bs_gpu, bs_t_gpu, K, N
+                );
+            }
+
+            {
+                const size_t global_tiles_x = (M + warp_size - 1) / warp_size;
+                const size_t global_tiles_y = (N + warp_size - 1) / warp_size;
+
+                const size_t global_work_size_x = global_tiles_x * work_group_side;
+                const size_t global_work_size_y = global_tiles_y * work_group_side;
+
+                matrix_multiplication_kernel.exec(
+                    gpu::WorkSize(work_group_side, work_group_side, global_work_size_y, global_work_size_x), 
+                    as_gpu, bs_t_gpu, cs_gpu, M, K, N
+                );
+            }
 
             t.nextLap();
         }
@@ -85,14 +132,14 @@ int main(int argc, char **argv)
     }
 
     cs_gpu.readN(cs.data(), M*N);
-    */
 
+#ifdef WITH_CPU
     // Проверяем корректность результатов
     double diff_sum = 0;
     for (int i = 0; i < M * N; ++i) {
         double a = cs[i];
         double b = cs_cpu_reference[i];
-        if (a != 0.0 && b != 0.0) {
+        if (a != 0.0 || b != 0.0) {
             double diff = fabs(a - b) / std::max(fabs(a), fabs(b));
             diff_sum += diff;
         }
@@ -104,6 +151,7 @@ int main(int argc, char **argv)
         std::cerr << "Too big difference!" << std::endl;
         return 1;
     }
+#endif
 
     return 0;
 }
