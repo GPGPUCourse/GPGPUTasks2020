d654d11752154b268a577737264125606c667462
F5DXWsqPme <54502165+F5DXWsqPme@users.noreply.github.com>
2020-10-19 13:38:54 +0300

    Radix first rev

--- src/cl/radix.cl
+++ src/cl/radix.cl
@@ -1,4 +1,140 @@
-__kernel void radix(__global unsigned int* as)
+// BLOCK_SIZE_CD >= POW2_VALUE
+
+__kernel void CountDigits( __global const unsigned int* Array, __global unsigned int* Dst,
+                           unsigned int n, unsigned int DstSize, unsigned int Power2 )
 {
-    // TODO
+  __local unsigned int LocalArray[GROUP_SIZE * BLOCK_SIZE_CD];
+
+  for (unsigned int i = 0; i < BLOCK_SIZE_CD; i++)
+  {
+    const unsigned int Index = get_group_id(0) * GROUP_SIZE * BLOCK_SIZE_CD +
+                               i * GROUP_SIZE + get_local_id(0);
+
+    if (Index >= n)
+      break;
+
+    LocalArray[i * GROUP_SIZE + get_local_id(0)] = Array[Index];
+  }
+
+  barrier(CLK_LOCAL_MEM_FENCE);
+
+  unsigned int ResCount[POW2_VALUE] = {};
+
+  for (unsigned int i = 0; i < BLOCK_SIZE_CD; i++)
+  {
+    const unsigned int Index = BLOCK_SIZE_CD * get_local_id(0) + i;
+
+    if (get_group_id(0) * GROUP_SIZE * BLOCK_SIZE_CD + Index >= n)
+      break;
+
+    ResCount[(LocalArray[Index] >> Power2) & MASK]++;
+  }
+
+  if (get_global_id(0) >= DstSize)
+    return;
+
+  for (unsigned int i = 0; i < POW2_VALUE; i++)
+    Dst[DstSize * i + get_global_id(0)] = ResCount[i];
+}
+
+__kernel void EvalOffsets( __global const unsigned int* Array, unsigned int OffsetA,
+                           __global unsigned int* Dst, unsigned int OffsetD,
+                           unsigned int DstSize, unsigned int n )
+{
+  __local unsigned int LocalArray[GROUP_SIZE * BLOCK_SIZE_EO * POW2_VALUE];
+
+  for (unsigned int i = 0; i < POW2_VALUE; i++)
+    for (unsigned int j = 0; j < BLOCK_SIZE_EO; j++)
+    {
+      const unsigned int Index = i * n + get_group_id(0) * GROUP_SIZE * BLOCK_SIZE_EO +
+                                 j * GROUP_SIZE + get_local_id(0);
+
+      if (get_group_id(0) * GROUP_SIZE * BLOCK_SIZE_EO + j * GROUP_SIZE + get_local_id(0) >= n)
+        break;
+
+      LocalArray[i * GROUP_SIZE * BLOCK_SIZE_EO + j * GROUP_SIZE + get_local_id(0)] = Array[OffsetA + Index];
+    }
+
+  barrier(CLK_LOCAL_MEM_FENCE);
+
+  unsigned int ResOffset[POW2_VALUE];
+
+  for (unsigned int i = 0; i < POW2_VALUE; i++)
+  {
+    ResOffset[i] = 0;
+
+    for (unsigned int j = 0; j < BLOCK_SIZE_EO; j++)
+    {
+      if (get_group_id(0) * GROUP_SIZE * BLOCK_SIZE_EO + j + get_local_id(0) * BLOCK_SIZE_EO >= n)
+        break;
+
+      ResOffset[i] += LocalArray[i * GROUP_SIZE * BLOCK_SIZE_EO + j + get_local_id(0) * BLOCK_SIZE_EO];
+    }
+  }
+
+  if (get_global_id(0) < DstSize)
+    for (unsigned int i = 0; i < POW2_VALUE; i++)
+      Dst[OffsetD + DstSize * i + get_global_id(0)] = ResOffset[i];
+}
+
+// BLOCK_SIZE_EO = 2
+
+__kernel void Radix( __global const unsigned int* Array, __global unsigned int* Dst,
+                     __global const unsigned int *Offsets,
+                     __global const unsigned int *Sums, unsigned int n, unsigned int Power2,
+                     unsigned int SumsInd )
+{
+  unsigned int IndForOffsets = get_global_id(0);
+  unsigned int GlobalOffsets[POW2_VALUE] = {};
+  unsigned int OffsetIndex = 0;
+
+  if (get_global_id(0) * BLOCK_SIZE_CD < n)
+  {
+    while (IndForOffsets > 0)
+    {
+      unsigned int IndDigit = (IndForOffsets & 1);
+
+      if (IndDigit)
+        for (unsigned int i = 0; i < POW2_VALUE; i++)
+          GlobalOffsets[i] += Sums[Offsets[OffsetIndex * POW2_VALUE + i] + IndForOffsets - 1];
+
+      IndForOffsets >>= 1;
+      OffsetIndex++;
+    }
+
+    unsigned int GlobalSum = 0;
+
+    for (unsigned int i = 1; i < POW2_VALUE; i++)
+    {
+      GlobalSum += Sums[SumsInd + i - 1];
+      GlobalOffsets[i] += GlobalSum;
+    }
+  }
+
+  __local unsigned int LocalArray[GROUP_SIZE * BLOCK_SIZE_CD];
+
+  for (unsigned int i = 0; i < BLOCK_SIZE_CD; i++)
+  {
+    const unsigned int Index = get_group_id(0) * GROUP_SIZE * BLOCK_SIZE_CD +
+                               i * GROUP_SIZE + get_local_id(0);
+
+    if (Index >= n)
+      break;
+
+    LocalArray[i * GROUP_SIZE + get_local_id(0)] = Array[Index];
+  }
+
+  barrier(CLK_LOCAL_MEM_FENCE);
+
+  for (unsigned int i = 0; i < BLOCK_SIZE_CD; i++)
+  {
+    const unsigned int Index = BLOCK_SIZE_CD * get_local_id(0) + i;
+
+    if (get_group_id(0) * GROUP_SIZE * BLOCK_SIZE_CD + Index >= n)
+      break;
+
+    unsigned int Elem = LocalArray[Index];
+
+    Dst[GlobalOffsets[(Elem >> Power2) & MASK]++] = Elem;
+  }
 }
--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -23,6 +23,37 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+const unsigned int RadixCPUPow2 = 8;
+const unsigned int RadixCPUPow2Value = 1 << RadixCPUPow2;
+const unsigned int RadixCPUMask = RadixCPUPow2Value - 1;
+const unsigned int RadixCPUNumOfIterations = (sizeof(unsigned int) * 8 + RadixCPUPow2 - 1) / RadixCPUPow2;
+
+void RadixSortCPU( unsigned int *A, unsigned int N, unsigned int *B, bool *ResInA )
+{
+  unsigned int Count[RadixCPUPow2Value];
+  unsigned int Offset[RadixCPUPow2Value];
+  unsigned int *Src = A;
+  unsigned int *Dst = B;
+
+  for (unsigned int Power2 = 0, It = 0; It < RadixCPUNumOfIterations; Power2 += RadixCPUPow2, It++)
+  {
+    memset(Count, 0, sizeof(Count));
+
+    for (unsigned int i = 0; i < N; i++)
+      Count[(Src[i] >> Power2) & RadixCPUMask]++;
+
+    Offset[0] = 0;
+    for (unsigned int i = 1; i < RadixCPUPow2Value; i++)
+      Offset[i] = Offset[i - 1] + Count[i - 1];
+    
+    for (unsigned int i = 0; i < N; i++)
+      Dst[Offset[(Src[i] >> Power2) & RadixCPUMask]++] = Src[i];
+
+    std::swap(Src, Dst);
+  }
+
+  *ResInA = RadixCPUNumOfIterations % 2 == 0;
+}
 
 int main(int argc, char **argv)
 {
@@ -32,7 +63,7 @@ int main(int argc, char **argv)
     context.init(device.device_id_opencl);
     context.activate();
 
-    int benchmarkingIters = 10;
+    int benchmarkingIters = 1;
     unsigned int n = 32 * 1024 * 1024;
     std::vector<unsigned int> as(n, 0);
     FastRandom r(n);
@@ -46,19 +77,74 @@ int main(int argc, char **argv)
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             cpu_sorted = as;
+            t.restart();
             std::sort(cpu_sorted.begin(), cpu_sorted.end());
             t.nextLap();
         }
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-/*
-    gpu::gpu_mem_32u as_gpu;
-    as_gpu.resizeN(n);
 
     {
-        ocl::Kernel radix(radix_kernel, radix_kernel_length, "radix");
-        radix.compile();
+        std::vector<unsigned int> RadixCpuA;
+        std::vector<unsigned int> RadixCpuB(n);
+        bool ResInA;
+        timer t;
+        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+            RadixCpuA = as;
+            t.restart();
+            RadixSortCPU(RadixCpuA.data(), n, RadixCpuB.data(), &ResInA);
+            t.nextLap();
+        }
+        std::cout << "CPU Radix: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "CPU Radix: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
+
+        std::vector<unsigned int> &Res = ResInA ? RadixCpuA : RadixCpuB;
+
+        for (int i = 0; i < n; ++i) {
+          EXPECT_THE_SAME(cpu_sorted[i], Res[i], "CPU Radix results should be equal to CPU results!");
+        }
+    }
+
+    gpu::gpu_mem_32u as_gpu;
+    as_gpu.resizeN(n);
+    gpu::gpu_mem_32u ResGPU;
+    ResGPU.resizeN(n);
+
+    {
+        const unsigned int Pow2 = 4;
+        const unsigned int Pow2Value = 1 << Pow2;
+        const unsigned int Mask = Pow2Value - 1;
+        const unsigned int NumOfIterations = (sizeof(unsigned int) * 8 + Pow2 - 1) / Pow2;
+        
+        const unsigned int WorkGroupSize = 64;
+        const unsigned int BlockSizeCD = 60;
+        const unsigned int BlockSizeEO = 2;
+
+        unsigned int CellSize = 1;
+
+        std::string Defines = "-D POW2_VALUE=" + std::to_string(Pow2Value) + " ";
+        Defines += "-D POW2=" + std::to_string(Pow2) + " ";
+        Defines += "-D MASK=" + std::to_string(Mask) + " ";
+        Defines += "-D GROUP_SIZE=" + std::to_string(WorkGroupSize) + " ";
+        Defines += "-D BLOCK_SIZE_CD=" + std::to_string(BlockSizeCD) + " ";
+        Defines += "-D BLOCK_SIZE_EO=" + std::to_string(BlockSizeEO);
+
+        ocl::Kernel CountDigits(radix_kernel, radix_kernel_length, "CountDigits", Defines);
+        CountDigits.compile();
+        ocl::Kernel EvalOffsets(radix_kernel, radix_kernel_length, "EvalOffsets", Defines);
+        EvalOffsets.compile();
+        ocl::Kernel Radix(radix_kernel, radix_kernel_length, "Radix", Defines);
+        Radix.compile();
+
+        gpu::gpu_mem_32u OffsetsGPU;
+        gpu::gpu_mem_32u CountDigitsGPU;
+
+        gpu::gpu_mem_32u *Src;
+        gpu::gpu_mem_32u *Dst;
+
+        std::vector<unsigned int> Offsets;
+        Offsets.reserve(Pow2Value * (unsigned int)log2f(n) / Pow2 + Pow2Value * Pow2);
 
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
@@ -66,22 +152,75 @@ int main(int argc, char **argv)
 
             t.restart(); // Запускаем секундомер после прогрузки данных чтобы замерять время работы кернела, а не трансфер данных
 
-            unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            radix.exec(gpu::WorkSize(workGroupSize, global_work_size),
-                       as_gpu, n);
+            Offsets.clear();
+
+            unsigned int SumArraySize = Pow2Value;
+            unsigned int CurN = (n + BlockSizeCD - 1) / BlockSizeCD;
+            unsigned int CurOffset = 0;
+
+            while (CurN > 1)
+            {
+              SumArraySize += CurN * Pow2Value;
+
+              for (unsigned int i = 0; i < Pow2Value; i++)
+              {
+                Offsets.push_back(CurOffset);
+                CurOffset += CurN;
+              }
+
+              CurN = (CurN + BlockSizeEO - 1) / BlockSizeEO;
+            }
+
+            OffsetsGPU.resizeN(Offsets.size());
+            OffsetsGPU.writeN(Offsets.data(), Offsets.size());
+            CountDigitsGPU.resizeN(SumArraySize);
+
+            Src = &as_gpu;
+            Dst = &ResGPU;
+
+            for (unsigned int Power2 = 0, It = 0; It < NumOfIterations; Power2 += Pow2, It++)
+            {
+              CurN = (n + BlockSizeCD - 1) / BlockSizeCD;
+
+              unsigned int GlobalWorkSizeCD = (n + WorkGroupSize * BlockSizeCD - 1) / (WorkGroupSize * BlockSizeCD) * WorkGroupSize;
+              CountDigits.exec(gpu::WorkSize(WorkGroupSize, GlobalWorkSizeCD),
+                               *Src, CountDigitsGPU, n, CurN, Power2);
+
+              CurOffset = 0;
+
+              while (CurN > 1)
+              {
+                CurOffset += CurN * Pow2Value;
+
+                unsigned int GlobalWorkSizeEO = (CurN + WorkGroupSize * BlockSizeEO - 1) / (WorkGroupSize * BlockSizeEO) * WorkGroupSize;
+
+                EvalOffsets.exec(gpu::WorkSize(WorkGroupSize, GlobalWorkSizeEO),
+                                 CountDigitsGPU, CurOffset - CurN * Pow2Value,
+                                 CountDigitsGPU, CurOffset,
+                                 (CurN + BlockSizeEO - 1) / BlockSizeEO, CurN);
+
+                CurN = (CurN + BlockSizeEO - 1) / BlockSizeEO;
+              }
+
+              Radix.exec(gpu::WorkSize(WorkGroupSize, GlobalWorkSizeCD),
+                         *Src, *Dst,
+                         OffsetsGPU, CountDigitsGPU, n, Power2, CurOffset);
+
+              std::swap(Src, Dst);
+            }
+
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
 
-        as_gpu.readN(as.data(), n);
+        Src->readN(as.data(), n);
     }
 
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
