ef46d5da967765666c55f32bba2a2016459fcf97
Daniil Smirnov <dansmi97@gmail.com>
2020-10-26 07:08:44 +0300

    Implemented base version of radix

--- src/cl/radix.cl
+++ src/cl/radix.cl
@@ -1,4 +1,99 @@
-__kernel void radix(__global unsigned int* as)
-{
-    // TODO
+__kernel void radix_setup(
+    const __global unsigned int *as, 
+    const unsigned int n, 
+    __global unsigned int *counts,
+    const unsigned int bit
+) {
+    const size_t iGlobal = get_global_id(0);
+    const size_t iGroup = get_local_id(0);
+    
+    if (iGlobal == 0) {
+        counts[0] = 0;
+    }
+
+    if (iGlobal < n) {
+        counts[iGlobal + 1] = (as[iGlobal] >> bit) & 1;
+    } else {
+        counts[iGlobal + 1] = 0;
+    }
+}
+
+__kernel void radix_gather(
+    __global unsigned int *as, 
+    const unsigned int n,
+    const unsigned int globalStep
+) {
+    const size_t iGlobal = get_global_id(0) * globalStep;
+    const size_t iGroup = get_local_id(0);
+    
+    __local unsigned int asLocal[LOCAL_SIZE];
+   
+    if (iGlobal < n) {
+        asLocal[iGroup] = as[iGlobal];
+    } else {
+        asLocal[iGroup] = 0;
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    for (size_t step = 1; step < LOCAL_SIZE; step <<= 1) {
+        if (iGroup >= step) {
+            asLocal[iGroup] += asLocal[iGroup - step];
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+    if (iGlobal < n) {
+        as[iGlobal] = asLocal[iGroup];
+    }
+}
+
+__kernel void radix_propagate(
+    __global unsigned int *as, 
+    const unsigned int n
+) {
+    const size_t iGlobal = get_global_id(0);
+    const size_t groupOffset = get_global_offset(0);
+
+    if (iGlobal % LOCAL_SIZE) {
+        as[iGlobal] += as[groupOffset];
+    }
+}
+
+__kernel void radix_move(
+    const __global unsigned int *as, 
+    const unsigned int n, 
+    const __global unsigned int *counts, 
+    __global unsigned int *bs
+) {
+    const size_t iGroup = get_group_id(0);
+    const size_t groupOffset = get_global_offset(0);
+    const size_t iGlobal = groupOffset + iGroup;
+    
+    const size_t ones = counts[n];
+    const size_t zeros = n - ones;
+
+    const size_t isZero = iGlobal + ones < n;
+    const size_t indexToSearch = isZero ? iGlobal - counts[iGlobal] : zeros + counts[iGlobal];
+    
+    size_t l = 0;
+    size_t r = n + 1;
+    while (l + 1 < r) {
+        const size_t m = (l + r) >> 1;
+        
+        size_t index;
+        if (isZero) {
+            index = m - counts[m];
+        } else {
+            index = zeros + counts[m];
+        }
+
+        if (index > indexToSearch) {
+            r = m;
+        } else {
+            l = m;
+        }
+    }
+    barrier(CLK_GLOBAL_MEM_FENCE); // just to synchronize everything
+
+    bs[iGlobal] = as[l - 1];
 }
--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -13,16 +13,27 @@
 
 
 template<typename T>
-void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
+void raiseFail(size_t i, const T &a, const T &b, std::string message, std::string filename, int line)
 {
     if (a != b) {
-        std::cerr << message << " But " << a << " != " << b << ", " << filename << ":" << line << std::endl;
+        std::cerr << message << " But " << a << " != " << b << " at " << i << ", " << filename << ":" << line << std::endl;
         throw std::runtime_error(message);
     }
 }
 
-#define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
+#define EXPECT_THE_SAME(i, a, b, message) raiseFail(i, a, b, message, __FILE__, __LINE__)
 
+#define PREVIEW 16
+
+template<typename T>
+void preview(const std::vector<T> &a) {
+#ifdef PREVIEW
+    for (size_t i = 0; i < std::min(a.size(), (size_t) PREVIEW); ++i) {
+        std::cout << a[i] << " ";
+    }
+    std::cout << "\n";
+#endif
+}
 
 int main(int argc, char **argv)
 {
@@ -32,12 +43,12 @@ int main(int argc, char **argv)
     context.init(device.device_id_opencl);
     context.activate();
 
-    int benchmarkingIters = 10;
-    unsigned int n = 32 * 1024 * 1024;
+    int benchmarkingIters = 1;//10;
+    unsigned int n = 16;//32 * 1024 * 1024;
     std::vector<unsigned int> as(n, 0);
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
-        as[i] = (unsigned int) r.next(0, std::numeric_limits<int>::max());
+        as[i] = (unsigned int) r.next(0, 16);//std::numeric_limits<int>::max());
     }
     std::cout << "Data generated for n=" << n << "!" << std::endl;
 
@@ -52,13 +63,34 @@ int main(int argc, char **argv)
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-/*
+    preview(cpu_sorted);
+
     gpu::gpu_mem_32u as_gpu;
     as_gpu.resizeN(n);
 
+    gpu::gpu_mem_32u counts_gpu;
+    counts_gpu.resizeN(n + 1);
+
+    gpu::gpu_mem_32u bs_gpu;
+    bs_gpu.resizeN(n);
+
     {
-        ocl::Kernel radix(radix_kernel, radix_kernel_length, "radix");
-        radix.compile();
+        unsigned int workGroupSize = 256;
+        unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
+        
+        const auto local_size_def = "-DLOCAL_SIZE=" + std::to_string(workGroupSize);
+
+        ocl::Kernel radix_setup(radix_kernel, radix_kernel_length, "radix_setup", local_size_def);
+        radix_setup.compile();
+
+        ocl::Kernel radix_gather(radix_kernel, radix_kernel_length, "radix_gather", local_size_def);
+        radix_gather.compile();
+
+        ocl::Kernel radix_propagate(radix_kernel, radix_kernel_length, "radix_propagate", local_size_def);
+        radix_propagate.compile();
+
+        ocl::Kernel radix_move(radix_kernel, radix_kernel_length, "radix_move", local_size_def);
+        radix_move.compile();
 
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
@@ -66,10 +98,29 @@ int main(int argc, char **argv)
 
             t.restart(); // Запускаем секундомер после прогрузки данных чтобы замерять время работы кернела, а не трансфер данных
 
-            unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            radix.exec(gpu::WorkSize(workGroupSize, global_work_size),
-                       as_gpu, n);
+            for (size_t bit = 0; bit < sizeof(unsigned int) * 8; ++bit) {
+                radix_setup.exec(
+                    gpu::WorkSize(workGroupSize, global_work_size),
+                    bit % 2 ? bs_gpu : as_gpu, n, counts_gpu, (unsigned int) bit
+                );
+
+                for (size_t step = 1; step <= n + 1; step *= workGroupSize) {
+                    radix_gather.exec(
+                        gpu::WorkSize(workGroupSize, global_work_size),
+                        counts_gpu, n, (unsigned int) step
+                    );
+                }
+
+                radix_propagate.exec(
+                    gpu::WorkSize(workGroupSize, global_work_size),
+                    counts_gpu, n
+                );
+
+                radix_move.exec(
+                    gpu::WorkSize(workGroupSize, global_work_size),
+                    bit % 2 ? bs_gpu : as_gpu, n, counts_gpu, bit % 2 ? as_gpu : bs_gpu
+                );
+            }
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
@@ -77,11 +128,11 @@ int main(int argc, char **argv)
 
         as_gpu.readN(as.data(), n);
     }
+    preview(as);
 
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
-        EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
+        EXPECT_THE_SAME(i, as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
     return 0;
 }
