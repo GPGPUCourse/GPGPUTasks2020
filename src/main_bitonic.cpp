aa5ce8a992c166b4d4e23024c98662b595734b3b
Tur Vsevolod <60890623+turvd@users.noreply.github.com>
2020-10-26 19:42:13 +0300

    Add files via upload

--- src/main_bitonic.cpp
+++ src/main_bitonic.cpp
@@ -52,13 +52,15 @@ int main(int argc, char **argv)
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-/*
+
     gpu::gpu_mem_32f as_gpu;
     as_gpu.resizeN(n);
 
     {
-        ocl::Kernel bitonic(bitonic_kernel, bitonic_kernel_length, "bitonic");
-        bitonic.compile();
+        ocl::Kernel bitonic_global(bitonic_kernel, bitonic_kernel_length, "bitonic_global");
+		ocl::Kernel bitonic_local(bitonic_kernel, bitonic_kernel_length, "bitonic_local");
+        bitonic_global.compile();
+		bitonic_local.compile();
 
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
@@ -66,10 +68,17 @@ int main(int argc, char **argv)
 
             t.restart(); // Запускаем секундомер после прогрузки данных чтобы замерять время работы кернела, а не трансфер данных
 
-            unsigned int workGroupSize = 128;
+            unsigned int workGroupSize = 256;
             unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            bitonic.exec(gpu::WorkSize(workGroupSize, global_work_size),
-                         as_gpu, n);
+            for(int step=1;step<n;step*=2)
+			{
+				for(int local_step=step;local_step>workGroupSize;local_step/=2)
+				{
+					bitonic_global.exec(gpu::WorkSize(workGroupSize, global_work_size),as_gpu,n,step,local_step);
+					// Вместо barrier(CLK_GLOBAL_MEM_FENCE)
+				}
+				bitonic_local.exec(gpu::WorkSize(workGroupSize, global_work_size),as_gpu,n,step);
+			}
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
@@ -79,9 +88,11 @@ int main(int argc, char **argv)
     }
 
     // Проверяем корректность результатов
+	//for (int i = 0; i < n; ++i)
+	//	std::cout<<as[i]<<' ';
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -32,7 +32,7 @@ int main(int argc, char **argv)
     context.init(device.device_id_opencl);
     context.activate();
 
-    int benchmarkingIters = 10;
+    int benchmarkingIters = 1;///10;
     unsigned int n = 32 * 1024 * 1024;
     std::vector<unsigned int> as(n, 0);
     FastRandom r(n);
@@ -52,36 +52,100 @@ int main(int argc, char **argv)
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-/*
-    gpu::gpu_mem_32u as_gpu;
-    as_gpu.resizeN(n);
 
     {
-        ocl::Kernel radix(radix_kernel, radix_kernel_length, "radix");
+        unsigned int WG_SIZE=256;
+		unsigned int WG_COUNT=n/WG_SIZE;
+		gpu::gpu_mem_32u tmp[2];
+		tmp[0].resizeN(n);
+		tmp[1].resizeN(n);
+		gpu::gpu_mem_32u prefix_sums;
+		prefix_sums.resizeN(WG_COUNT);
+		
+		ocl::Kernel radix(radix_kernel, radix_kernel_length, "radix");
+		ocl::Kernel prefix_local(radix_kernel, radix_kernel_length, "prefix_local");
+		ocl::Kernel prefix_global(radix_kernel, radix_kernel_length, "prefix_global");
+		ocl::Kernel prefix_global_256(radix_kernel, radix_kernel_length, "prefix_global_256");
         radix.compile();
+        prefix_local.compile();
+        prefix_global.compile();
+		prefix_global_256.compile();
 
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            as_gpu.writeN(as.data(), n);
+            tmp[0].writeN(as.data(), n);
 
             t.restart(); // Запускаем секундомер после прогрузки данных чтобы замерять время работы кернела, а не трансфер данных
-
-            unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            radix.exec(gpu::WorkSize(workGroupSize, global_work_size),
-                       as_gpu, n);
+            for(int bit_pos=0;bit_pos<32;bit_pos++)
+			{
+                prefix_local.exec(gpu::WorkSize(WG_SIZE, n), tmp[bit_pos%2], prefix_sums, n, bit_pos);
+                prefix_global_256.exec(gpu::WorkSize(WG_SIZE, WG_COUNT), prefix_sums, WG_COUNT);
+                for(int step=WG_SIZE;step<WG_COUNT;step*=2)
+                    prefix_global.exec(gpu::WorkSize(WG_SIZE, WG_COUNT), prefix_sums, WG_COUNT, step);
+                radix.exec(gpu::WorkSize(WG_SIZE, n), tmp[bit_pos%2], tmp[1-bit_pos%2], prefix_sums, n, bit_pos);
+            }
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
 
-        as_gpu.readN(as.data(), n);
+        tmp[0].readN(as.data(), n);
+    }
+
+    // Проверяем корректность результатов
+	//for (int i = 0; i < n; ++i)
+	//	std::cout<<as[i]<<','<<cpu_sorted[i]<<'\n';
+    for (int i = 0; i < n; ++i) {
+        EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
+    }
+	
+	/*
+	//optimize
+	
+	{
+        unsigned int WG_SIZE=256;
+		unsigned int WG_COUNT=n/WG_SIZE/2;
+		gpu::gpu_mem_32u tmp[2];
+		tmp[0].resizeN(n);
+		tmp[1].resizeN(n);
+		gpu::gpu_mem_32u prefix_sums;
+		prefix_sums.resizeN(WG_COUNT);
+		
+		ocl::Kernel radix_2(radix_kernel, radix_kernel_length, "radix_2");
+		ocl::Kernel prefix_local_2(radix_kernel, radix_kernel_length, "prefix_local_2");
+		ocl::Kernel prefix_global_2(radix_kernel, radix_kernel_length, "prefix_global_2");
+		ocl::Kernel prefix_global_256_2(radix_kernel, radix_kernel_length, "prefix_global_256_2");
+        radix_2.compile();
+        prefix_local_2.compile();
+        prefix_global_2.compile();
+		prefix_global_256_2.compile();
+
+        timer t;
+        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+            tmp[0].writeN(as.data(), n);
+
+            t.restart(); // Запускаем секундомер после прогрузки данных чтобы замерять время работы кернела, а не трансфер данных
+            for(unsigned int bit_pos=0;bit_pos<32;bit_pos++)
+			{
+                prefix_local_2.exec(gpu::WorkSize(WG_SIZE, n/2), tmp[bit_pos%2], prefix_sums, n, bit_pos);
+                prefix_global_256_2.exec(gpu::WorkSize(WG_SIZE, WG_COUNT), prefix_sums, WG_COUNT);
+                for(unsigned int step=WG_SIZE;step<WG_COUNT;step*=2)
+                    prefix_global_2.exec(gpu::WorkSize(WG_SIZE, WG_COUNT), prefix_sums, WG_COUNT, step);
+                radix_2.exec(gpu::WorkSize(WG_SIZE, n/2), tmp[bit_pos%2], tmp[1-bit_pos%2], prefix_sums, n, bit_pos);
+            }
+            t.nextLap();
+        }
+        std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "GPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
+
+        tmp[0].readN(as.data(), n);
     }
 
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+	*/
+
     return 0;
 }
