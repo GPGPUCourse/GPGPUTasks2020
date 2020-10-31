bb3e128c4b2e8d8b06edf404536bb4ae2c3952a7
Scherbakov, Maksim <maksim.scherbakov@wartsila.com>
2020-11-01 01:07:40 +0300

    task03 - improve sum

--- src/cl/sum.cl
+++ src/cl/sum.cl
@@ -3,32 +3,62 @@
 #endif
 
 #line 6
-#define WORK_GROUP_SIZE 128
-__kernel void sum(__global const unsigned int* numbers,
+
+__kernel void sum(__global const unsigned int* array,
                   __global unsigned int* res,
                   unsigned int n)
 {
     int local_id = get_local_id(0);
     int global_id = get_global_id(0);
+    int group_id = get_group_id(0);
 
-    __local int local_xs[WORK_GROUP_SIZE];
-    if (global_id >= n) {
-        local_xs[local_id] = 0;
+    __local int local_array[WORK_GROUP_SIZE];
+    if (global_id < n) {
+        local_array[local_id] = array[global_id];
     } else {
-        local_xs[local_id] = numbers[global_id];
+        local_array[local_id] = 0;
     }
 
 
     barrier(CLK_LOCAL_MEM_FENCE);
     for (int n_values = WORK_GROUP_SIZE; n_values > 1; n_values /= 2) {
         if (2 * local_id < n_values) {
-            unsigned int a = local_xs[local_id];
-            unsigned int b = local_xs[local_id + n_values / 2];
-            local_xs[local_id] = a + b;
+            unsigned int a = local_array[local_id];
+            unsigned int b = local_array[local_id + n_values / 2];
+            local_array[local_id] = a + b;
         }
         barrier(CLK_LOCAL_MEM_FENCE);
     }
     if (local_id == 0) {
-        atomic_add(res, local_xs[0]);
+        res[group_id] = local_array[0];
     }
+}
+
+ __kernel void sum_tree(__global const int* array,
+                   __global int* sum_array,
+                   unsigned int n) {
+    int local_id = get_local_id(0);
+    int global_id = get_global_id(0);
+    int group_id = get_group_id(0);
+
+    __local int local_array[WORK_GROUP_SIZE];
+    if (global_id < n) {
+        local_array[local_id] = array[global_id];
+    } else {
+        local_array[local_id] = 0;
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+    int sum = 0;
+    for (int n_values = WORK_GROUP_SIZE; n_values > 1; n_values /= 2) {
+        if (local_id * 2 < n_values) {
+            unsigned int a = local_array[local_id];
+            unsigned int b = local_array[local_id + n_values / 2];
+            local_array[local_id] = a + b;
+        }
+
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+    if (local_id == 0)
+        sum_array[group_id] = local_array[0];
 }
--- src/main_sum.cpp
+++ src/main_sum.cpp
@@ -67,28 +67,42 @@ int main(int argc, char **argv)
          gpu::Context context;
          context.init(device.device_id_opencl);
          context.activate();
-         ocl::Kernel kernel(sum_kernel, sum_kernel_length, "sum");
+
+         unsigned int wg_size = 32;
+
+         std::string defines = "-D WORK_GROUP_SIZE=" + std::to_string(wg_size);
+         ocl::Kernel kernel(sum_kernel, sum_kernel_length, "sum_tree", defines);
 
          bool print_log{false};
          kernel.compile(print_log);
-         unsigned int work_group_size = 128;
 
-         unsigned int global_work_size = (n + work_group_size - 1) / work_group_size * work_group_size;
+         unsigned int global_work_size = (n + wg_size - 1) / wg_size * wg_size;
          timer t;
          gpu::gpu_mem_32u as_gpu;
          as_gpu.resizeN(global_work_size);
          as_gpu.writeN(as.data(), n);
-         gpu::gpu_mem_32u sum_gpu;
-         sum_gpu.resizeN(1);
 
-         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-             unsigned int sum = 0;
-             sum_gpu.writeN(&sum, 1);
-             kernel.exec(gpu::WorkSize(work_group_size, global_work_size),
-                         as_gpu, sum_gpu, n);
-             sum_gpu.readN(&sum, 1);
-             EXPECT_THE_SAME(reference_sum, sum, "GPU result should be consistent!");
-             t.nextLap();
+        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+            gpu::gpu_mem_32u bs_gpu;
+            bs_gpu.resizeN(n);
+
+            t.stop(); // there is really no need to take this time into account
+            as_gpu.copyToN(bs_gpu, n);
+            t.start();
+
+            unsigned int sum = 0;
+            gpu::gpu_mem_32u sum_gpu;
+            for (unsigned int size = global_work_size; size > 1; size = (size + wg_size - 1) / wg_size * wg_size / wg_size) {
+                const auto sum_size = size / wg_size;
+                sum_gpu.resizeN(0 < sum_size ? sum_size : 1);
+                kernel.exec(gpu::WorkSize(wg_size, size), bs_gpu, sum_gpu, size);
+
+                bs_gpu.swap(sum_gpu);
+            }
+
+            bs_gpu.readN(&sum, 1);
+            EXPECT_THE_SAME(reference_sum, sum, "GPU result should be consistent!");
+            t.nextLap();
         }
         std::cout << "GPU:     " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU:     " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
