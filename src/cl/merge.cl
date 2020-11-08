d8c97f77f67a082082544d458d3a95ec89418f9b
Daniil Smirnov <dansmi97@gmail.com>
2020-11-08 13:48:42 +0300

    Some initial code

--- src/cl/merge.cl
+++ src/cl/merge.cl
@@ -1,1 +1,43 @@
+#ifdef USE_LOCAL
+#   define from temp
+#   define to temp
+#   define i iLocal
+#else
+#   define from as
+#   define to as_next
+#   define i iGlobal
+#endif
 
+__kernel void merge(const __global float *as, __global float *as_next, const unsigned int n, unsigned int step) {
+    const unsigned int iGlobal = get_global_id(0);
+    const unsigned int iLocal = get_local_id(0);
+    
+#ifdef USE_LOCAL
+    __local float temp[LOCAL_SIZE];
+
+    temp[iLocal] = as[iGlobal];
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    step = 1;
+
+while (step < LOCAL_SIZE) {
+#endif
+
+    const unsigned int offset = i & (~((step << 1) - 1));
+    
+    // TODO
+
+#ifdef USE_LOCAL
+    barrier(CLK_LOCAL_MEM_FENCE);
+#endif
+    to[i] = foundElement;
+
+#ifdef USE_LOCAL
+    barrier(CLK_LOCAL_MEM_FENCE);
+    
+    step <<= 1;
+} // while
+
+    as_next[iGlobal] = temp[iLocal];
+#endif
+}
--- src/main_merge.cpp
+++ src/main_merge.cpp
@@ -23,6 +23,7 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+#define DEBUG 8
 
 int main(int argc, char **argv)
 {
@@ -32,14 +33,31 @@ int main(int argc, char **argv)
     context.init(device.device_id_opencl);
     context.activate();
 
+#ifdef DEBUG
+    int benchmarkingIters = 1;
+    unsigned int n = DEBUG;
+#else
     int benchmarkingIters = 10;
     unsigned int n = 32*1024*1024;
+#endif
+
     std::vector<float> as(n, 0);
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
+
+#ifdef DEBUG
+        as[i] = r.next(0, 16);
+#else
         as[i] = r.nextf();
+#endif
     }
     std::cout << "Data generated for n=" << n << "!" << std::endl;
+#ifdef DEBUG
+    for (const auto &x: as) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+#endif
 
     std::vector<float> cpu_sorted;
     {
@@ -52,30 +70,67 @@ int main(int argc, char **argv)
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-/*
-    gpu::gpu_mem_32f as_gpu;
-    as_gpu.resizeN(n);
+    
     {
-        ocl::Kernel merge(merge_kernel, merge_kernel_length, "merge");
-        merge.compile();
+        const unsigned int workGroupSize = 256;
+        const unsigned int global_work_size = [n, workGroupSize]() { // I'm tired of checking bounds all the time, 
+            unsigned int power = 1;                                  // so finding first power of 2 not smaller than needed
+            while (power < std::max(n, workGroupSize)) {             // worst case: n is 2^k - 1 for some k then the result would be 2^(k + 1)
+                power <<= 1;                                         //             then at most ~2n => we would experience ~2x slowdown  
+            }                                                        //             as we have n log^2(n) complexity
+
+            return power;
+        }();
+
+        { // padding
+            const auto maximum = *std::max_element(as.begin(), as.end());
+            std::cout << "padding the array with " << maximum << std::endl;
+            as.resize(global_work_size, maximum);
+        }
+
+        gpu::gpu_mem_32f as_gpu;
+        as_gpu.resizeN(global_work_size);
+        
+        gpu::gpu_mem_32f bs_gpu;
+        bs_gpu.resizeN(global_work_size);
+
+        const std::string defines = "-DLOCAL_SIZE=" + std::to_string(workGroupSize);
+
+        ocl::Kernel merge_local(merge_kernel, merge_kernel_length, "merge", defines + " -DUSE_LOCAL");
+        merge_local.compile();
+
+        ocl::Kernel merge_global(merge_kernel, merge_kernel_length, "merge", defines);
+        merge_global.compile();
+
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            as_gpu.writeN(as.data(), n);
+            as_gpu.writeN(as.data(), global_work_size);
             t.restart(); // Запускаем секундомер после прогрузки данных чтобы замерять время работы кернела, а не трансфер данных
-            unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            merge.exec(gpu::WorkSize(workGroupSize, global_work_size),
-                       as_gpu, n);
+
+            merge_local.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, as_gpu, n, 0);
+            
+            for (unsigned int step = workGroupSize; step < n; step <<= 1) {
+                merge_global.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, bs_gpu, n, step);
+                as_gpu.swap(bs_gpu);
+            }
+
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "GPU: " << (global_work_size/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
         as_gpu.readN(as.data(), n);
+#ifdef DEBUG
+        as.resize(n);
+        for (const auto &x: as) {
+            std::cout << x << " ";
+        }
+        std::cout << std::endl;
+#endif
     }
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
