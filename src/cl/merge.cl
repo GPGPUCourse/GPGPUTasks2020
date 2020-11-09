349058a158f194b3e9659ab587a7538ad3e21123
leoneed03 <mr.senukov@mail.ru>
2020-11-09 19:24:26 +0300

    prototype for merge sort (works slow)

--- src/cl/merge.cl
+++ src/cl/merge.cl
@@ -1,1 +1,63 @@
+#ifdef __CLION_IDE__
 
+#include <libgpu/opencl/cl/clion_defines.cl>
+
+#endif
+
+#line 6
+
+#define WORK_GROUP_SIZE 128
+
+__kernel void merge(__global const float *in, __global float *out, int len, int n) {
+
+    int id = get_global_id(0);
+    if (id >= n) {
+        return;
+    }
+
+    int left_block_start = id / (2 * len) * 2 * len;
+
+    if (left_block_start != id) {
+        return;
+    }
+
+    int right_block_start = left_block_start + len;
+
+    if (right_block_start >= n) {
+        for (int i = left_block_start; i < n; ++i) {
+            out[i] = in[i];
+        }
+        return;
+    }
+    if (len == 1) {
+        out[left_block_start] = min(in[left_block_start], in[left_block_start + 1]);
+        out[left_block_start + 1] = max(in[left_block_start], in[left_block_start + 1]);
+        return;
+    }
+
+    int pout = left_block_start;
+    int pl = left_block_start;
+    int pr = pl + len;
+    int right_bound = (left_block_start + 2 * len < n) ? (left_block_start + 2 * len) : (n);
+    for (pout = left_block_start; pout < right_bound; ++pout) {
+        if (pr >= right_bound) {
+            out[pout] = in[pl];
+            ++pl;
+            continue;
+        }
+        if (pl >= left_block_start + len) {
+            out[pout] = in[pr];
+            ++pr;
+            continue;
+        }
+        if (in[pl] < in[pr]) {
+            out[pout] = in[pl];
+            ++pl;
+        } else {
+            out[pout] = in[pr];
+            ++pr;
+        }
+    }
+
+    return;
+}
--- src/main_merge.cpp
+++ src/main_merge.cpp
@@ -13,8 +13,7 @@
 
 
 template<typename T>
-void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
-{
+void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line) {
     if (a != b) {
         std::cerr << message << " But " << a << " != " << b << ", " << filename << ":" << line << std::endl;
         throw std::runtime_error(message);
@@ -24,16 +23,15 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     gpu::Device device = gpu::chooseGPUDevice(argc, argv);
 
     gpu::Context context;
     context.init(device.device_id_opencl);
     context.activate();
 
-    int benchmarkingIters = 10;
-    unsigned int n = 32*1024*1024;
+    int benchmarkingIters = 1;
+    unsigned int n = /* 32 */ 1024 * 1024;
     std::vector<float> as(n, 0);
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
@@ -50,32 +48,42 @@ int main(int argc, char **argv)
             t.nextLap();
         }
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-/*
-    gpu::gpu_mem_32f as_gpu;
+
+    gpu::gpu_mem_32f as_gpu, as_gpu_out;
     as_gpu.resizeN(n);
+    as_gpu_out.resizeN(n);
     {
+        std::cout << "start compiling" << std::endl;
         ocl::Kernel merge(merge_kernel, merge_kernel_length, "merge");
         merge.compile();
+        std::cout << "end compiling" << std::endl;
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             as_gpu.writeN(as.data(), n);
             t.restart(); // Запускаем секундомер после прогрузки данных чтобы замерять время работы кернела, а не трансфер данных
             unsigned int workGroupSize = 128;
             unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            merge.exec(gpu::WorkSize(workGroupSize, global_work_size),
-                       as_gpu, n);
+            for (int power2 = 1; power2 < n; power2 *= 2) {
+//                std::cout << "SWAP                " << power2 << std::endl;
+                merge.exec(gpu::WorkSize(workGroupSize, global_work_size),
+                           as_gpu, as_gpu_out, power2, n);
+                std::swap(as_gpu, as_gpu_out);
+            }
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "GPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
         as_gpu.readN(as.data(), n);
     }
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
+        if (as[i] != cpu_sorted[i]) {
+            std::cout << i << " out of " << n << " " << as[i] << "!=" << cpu_sorted[i] << std::endl;
+        }
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
