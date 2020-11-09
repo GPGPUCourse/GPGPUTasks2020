4654b1625bf2129b0a00d57126b3792a438af2ac
Marucmp <agpec007@gmail.com>
2020-11-09 22:47:46 +0700

    initial

--- src/cl/merge.cl
+++ src/cl/merge.cl
@@ -1,1 +1,42 @@
+__kernel void merge(__global float* as,
+                    __global float* buf,
+                    int n, int l) {
 
+    const unsigned int id = get_global_id(0);
+    if (id >= n)
+        return;
+
+    // initialize box margins (horizontal/vertical)
+    int size = 2 * l;
+    int fr_v = id / size * size, to_v = min(n, fr_v + l);
+    // check if out the box
+    if (fr_v + l >= n) {
+        as[id] = buf[id];
+        return;
+    }
+    int fr_h = to_v, to_h = min(n, fr_h + l);
+//    printf("id=%d fr_v-%d to_v=%d fr_h=%d to_h=%d\n", id, fr_v, to_v, fr_h, to_h);
+
+    // find diagonal idx-s
+    int idx = id - fr_v;
+    int left = max(0, idx - (to_h - fr_h)) - 1;
+    int right = min(idx, l);
+//    printf("idx=%d left-%d right=%d\n", idx, left, right);
+
+    // binary search through diagonal el-s
+    while (left < right - 1) {
+
+        int mid = (left + right) >> 1;
+        if (as[fr_v+mid] <= as[fr_h-mid+idx-1])
+            left = mid;
+        else
+            right = mid;
+    }
+    // 1 -> 0 transfer idx-s
+    int found_v = fr_v + right;
+    int found_h = fr_h + idx - right;
+    // place minimum from corresponding numbers
+    bool less = found_v < to_v && as[found_v] <= as[found_h];
+    bool out = found_v < to_v && found_h >= to_h;
+    buf[id] =  (less || out) ? as[found_v] : as[found_h];
+}
--- src/main_merge.cpp
+++ src/main_merge.cpp
@@ -34,6 +34,8 @@ int main(int argc, char **argv)
 
     int benchmarkingIters = 10;
     unsigned int n = 32*1024*1024;
+//    int benchmarkingIters = 1;
+//    unsigned int n = 1*1024;
     std::vector<float> as(n, 0);
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
@@ -52,20 +54,27 @@ int main(int argc, char **argv)
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-/*
-    gpu::gpu_mem_32f as_gpu;
+
+    gpu::gpu_mem_32f as_gpu, buf;
     as_gpu.resizeN(n);
+    buf.resizeN(n);
     {
         ocl::Kernel merge(merge_kernel, merge_kernel_length, "merge");
-        merge.compile();
+        bool printLog = false;
+        merge.compile(printLog);
+
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             as_gpu.writeN(as.data(), n);
             t.restart(); // Запускаем секундомер после прогрузки данных чтобы замерять время работы кернела, а не трансфер данных
             unsigned int workGroupSize = 128;
             unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            merge.exec(gpu::WorkSize(workGroupSize, global_work_size),
-                       as_gpu, n);
+
+            for (int size = 1; size < n; size *= 2) {
+                merge.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, buf, n, size);
+                as_gpu.swap(buf);
+            }
+
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
@@ -76,6 +85,6 @@ int main(int argc, char **argv)
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
