637cc11a1c66dee84999d068e7bfa714b1df968f
Aleksandr Eslikov <eslikov@gmail.com>
2020-11-09 10:31:20 +0300

    Task06 Solution

--- src/cl/merge.cl
+++ src/cl/merge.cl
@@ -1,1 +1,101 @@
+__kernel void mergePath(__global float* as,
+                        __global float* bs,
+                        __global int* pathPoints,
+                        int aSize,
+                        int step) {
 
+    int frameSize = aSize;
+    int aIndex = 0;
+    int bIndex = 0;
+
+    bool topPart = get_global_id(0) < get_global_size(0) / 2;
+
+    int groupIndexOffset = get_group_id(0) * get_local_size(0);
+
+    // A
+    int aTopIndex = 0;
+    int aBottomIndex = (get_local_id(0) % (get_global_size(0) / 2) + 1);
+    aBottomIndex += groupIndexOffset;
+
+    if(!topPart) {
+        aTopIndex = get_global_id(0) % (get_global_size(0) / 2);
+        aBottomIndex = (get_global_size(0) / 2);
+    }
+
+    int aTop = aTopIndex * step + aIndex;
+    int aBottom = (aBottomIndex * step) + aIndex;
+
+    // B
+    int bTopIndex = 0;
+    int bBottomIndex = (get_local_id(0) % (get_global_size(0) / 2) + 1);
+    bBottomIndex += groupIndexOffset;
+    if(!topPart) {
+        bTopIndex = get_global_id(0) % (get_global_size(0) / 2);
+        bBottomIndex = (get_global_size(0) / 2);
+    }
+
+    int bTop = bIndex + (bTopIndex * step);
+    int bBottom = (bBottomIndex * step) + bIndex;
+
+    int midf = float(aBottom - aTop) / 2.0;
+    int mid = (aBottom - aTop) / 2;
+
+    while(mid > 0) {
+
+        int aI = aTop + mid;
+        int bI = bTop + mid;
+
+        if (as[aI] > bs[bI - 1]) {
+            if (as[aI - 1] <= bs[bI]) {
+                pathPoints[(get_global_id(0)  + 1) * 2] = aI;
+                pathPoints[(get_global_id(0)  + 1) * 2 + 1] = bI;
+                break;
+            } else {
+                aBottom = aI + 1;
+                bTop = bI - 1;
+            }
+        } else {
+            aTop = aI - 1;
+        }
+
+        midf = ceil(float(aBottom - aTop) / 2.0);
+        mid = int(midf);
+
+        if(mid == 1) {
+            pathPoints[(get_global_id(0) + 1) * 2] = aI;
+            pathPoints[(get_global_id(0) + 1) * 2 + 1] = bI;
+
+            break;
+        }
+    }
+}
+
+
+__kernel void mergeBlock(__global float* as,
+                         __global float* bs,
+                         __global float* merged,
+                         __global int* pathPoints,
+                         int aSize) {
+
+    int aIndex = pathPoints[get_global_id(0) * 2];
+    int bIndex = pathPoints[get_global_id(0) * 2 + 1];
+
+    int nextAIndex = min(pathPoints[get_global_id(0) * 2 + 2], aSize);
+    int nextBIndex = min(pathPoints[get_global_id(0) * 2 + 3], aSize);
+
+
+    int count = 0;
+    while((aIndex < nextAIndex || bIndex < nextBIndex) && count++ < 1000) {
+
+        float a = as[aIndex];
+        float b = bs[bIndex];
+
+        if((a < b && aIndex < aSize) || bIndex >= aSize) {
+            merged[aIndex + bIndex] = a;
+            aIndex++;
+        } else {
+            merged[aIndex + bIndex] = b;
+            bIndex++;
+        }
+    }
+}
--- src/main_merge.cpp
+++ src/main_merge.cpp
@@ -3,6 +3,9 @@
 #include <libutils/fast_random.h>
 #include <libgpu/context.h>
 #include <libgpu/shared_device_buffer.h>
+#include <iostream>
+#include <iomanip>
+#include "assert.h"
 
 // Этот файл будет сгенерирован автоматически в момент сборки - см. convertIntoHeader в CMakeLists.txt:18
 #include "cl/merge_cl.h"
@@ -23,6 +26,31 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+using namespace std;
+
+void printM(vector<float> as,
+            vector<float> bs,
+            vector<int> path) {
+
+    cout << setw(4) << " ";
+    for(int b = 0; b < bs.size(); b++) {
+        cout << setw(4) << bs[b];
+    }
+    cout << endl;
+
+    for(int a = 0; a < as.size(); a++) {
+        for(int b = 0; b < bs.size(); b++) {
+
+            if(b == 0) {
+                cout << setw(4) << as[a];
+            }
+            int idx = a * bs.size() + b;
+
+            cout << setw(4) << path[a * bs.size() + b];
+        }
+        cout << endl;
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -33,49 +61,95 @@ int main(int argc, char **argv)
     context.activate();
 
     int benchmarkingIters = 10;
-    unsigned int n = 32*1024*1024;
+    unsigned int n = 32 * 1024 * 1024;
     std::vector<float> as(n, 0);
+    std::vector<float> bs(n, 0);
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
         as[i] = r.nextf();
+        bs[i] = r.nextf();
     }
     std::cout << "Data generated for n=" << n << "!" << std::endl;
+
+    std::sort(as.begin(), as.end());
+    std::sort(bs.begin(), bs.end());
 
     std::vector<float> cpu_sorted;
     {
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            cpu_sorted = as;
-            std::sort(cpu_sorted.begin(), cpu_sorted.end());
+            cpu_sorted.resize(as.size() + bs.size());
+            merge(as.begin(), as.end(), bs.begin(), bs.end(), cpu_sorted.begin());
             t.nextLap();
         }
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-/*
+
+    std::vector<float> merged;
+    std::vector<int> path;
+
+    merged.resize(n * 2);
+
     gpu::gpu_mem_32f as_gpu;
     as_gpu.resizeN(n);
+
+    gpu::gpu_mem_32f bs_gpu;
+    bs_gpu.resizeN(n);
+
+    gpu::gpu_mem_32f merged_gpu;
+    merged_gpu.resizeN(merged.size());
     {
-        ocl::Kernel merge(merge_kernel, merge_kernel_length, "merge");
-        merge.compile();
         timer t;
+        ocl::Kernel merge(merge_kernel, merge_kernel_length, "mergePath");
+        merge.compile();
+
+        ocl::Kernel mergeBlock(merge_kernel, merge_kernel_length, "mergeBlock");
+        mergeBlock.compile();
+
+        as_gpu.writeN(as.data(), as.size());
+        bs_gpu.writeN(bs.data(), bs.size());
+
+        unsigned int workGroupSize = 256;
+        int NDRange = n / workGroupSize;
+        unsigned int global_work_size = (NDRange + workGroupSize - 1) / workGroupSize * workGroupSize;
+
+        path.resize(((n / workGroupSize) + 1) * 2);
+        path.assign(path.size(), 0);
+
+        gpu::gpu_mem_32i path_gpu;
+        path_gpu.resizeN(path.size());
+
+        path_gpu.writeN(path.data(), path.size());
+        merged_gpu.writeN(merged.data(), merged.size());
+
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            as_gpu.writeN(as.data(), n);
+
+            merged_gpu.writeN(merged.data(), merged.size());
             t.restart(); // Запускаем секундомер после прогрузки данных чтобы замерять время работы кернела, а не трансфер данных
-            unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
             merge.exec(gpu::WorkSize(workGroupSize, global_work_size),
-                       as_gpu, n);
+                       as_gpu,
+                       bs_gpu,
+                       path_gpu,
+                       as.size(),
+                       (2*n) / NDRange);
+
+            mergeBlock.exec(gpu::WorkSize(workGroupSize, NDRange + 1),
+                            as_gpu,
+                            bs_gpu,
+                            merged_gpu,
+                            path_gpu,
+                            as.size());
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << (n/1000/1000) / t.lapAvg() << " millions/s" << std::endl;
-        as_gpu.readN(as.data(), n);
+        merged_gpu.readN(merged.data(), merged.size());
     }
     // Проверяем корректность результатов
-    for (int i = 0; i < n; ++i) {
-        EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
+    for (int i = 0; i < merged.size(); ++i) {
+        EXPECT_THE_SAME(merged[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
